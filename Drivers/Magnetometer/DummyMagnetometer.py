4ed2be5698d541798b54dfdc48299a0ceb50203e
Carson Fox <carson.fox@aggiemail.usu.edu>
2019-09-18 17:42:43 -0600

    Tentative dummy accelerometer data. Don't know what magnetometer output would look like...

--- Drivers/Magnetometer/DummyMagnetometer.py
+++ Drivers/Magnetometer/DummyMagnetometer.py
@@ -1,9 +1,24 @@
+from datetime import datetime
+from math import sin, pi
+
 from Drivers.Driver import Driver
 
 
 class DummyMagnetometer(Driver):
     def __init__(self):
-        super().__init__('DummyMagnetometer', 1)
+        super().__init__('DummyMagnetometer', .33)
+        self.initial_time = datetime.now()
 
     def read(self):
-        return (1, 1, 1), (1, 1, 1)
+        # Trying to simulate a slow spin, using a periodic function.
+        # While spinning, the x and y axes should read between -1 and 1.
+
+        # f(x) = A*sin(Bx + C) -> A is the amplitude, 2π/B is the period, C is offset
+        x = (datetime.now() - self.initial_time).seconds
+        a = 1
+        b = 1
+        c = pi / 2
+
+        # No acceleration in y axis, periodic acceleration in x, z axes.
+        accel = (round(a * sin(b * x), 3), 0, round(a * sin(b * x + c), 3))
+        return accel, (1, 1, 1)
