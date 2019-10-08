fdd9d4892b4e12c17d222bfc96266a05e004877e
Scooter <scott.glaittli@aggiemail.usu.edu>
2019-10-08 16:04:45 -0600

    Added preliminary driver code

---
+++ Drivers/spy_camera/camera_driver.py
@@ -0,0 +1,17 @@
+class spy_camera:
+    def __init__(self):
+        pass # prolly hardware specific stuff
+
+    def take_picture():
+
+        filename = ''; # prolly something like <photoNumber>-<timeTaken>.ext
+        try:
+            # takePicture code here
+            # write file to filename in filesystem
+            pass
+        except:
+            # if picture take fails return NULL
+            pass
+        return filename;
+        
+
