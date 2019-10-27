d43b0ed56e74fc02b688f53f414a932ac681a653
Adam Alder <atariq11700@gmail.com>
2019-10-27 13:53:14 -0600

    updated the math

--- Drivers/uvDriver/uvDriver2.py
+++ Drivers/uvDriver/uvDriver2.py
@@ -1,20 +1,22 @@
 from Drivers.Driver import Driver
 
-
+    # TODO: change the driver class because it needs to get data from the adc driver
 class uvSensor(Driver):
     def __init__(self):
         super().__init__("uvSensor")
 
     #adcData is a 12 bit binary string
     def read(self,adcData):
-        # adc lsb found by Vrefin / 4096, 5V / 4096
+        # adc lsb found by Vrefin / 4096 which is 5V / 4096
         voltageStep = 0.001220703125
+
         # converts the 12 bit binary number to decimal
-        numberOfSteps = int(adcData, 2)
+        numberOfSteps = int(data, 2)
+
         # calculate the output voltage from UV sensor
-        voltage = round(numberOfSteps * voltageStep, 3)
-        # turn voltage into photocurrent
-        photoCurrent = round(voltage / 4.3 * 1000, 4)
-        # turn photocurrent into uv power in mW/cm^2
-        uvPower = round(photoCurrent/113,3)
-        return uvPower
+        voltage = numberOfSteps * voltageStep
+
+        # calculate the uvPower
+        uvPower = voltage * 1000 / 485.9
+        return(uvPower)
+
