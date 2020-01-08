e1bda617d14522a8311e8ad92c9a12f0b7fc07d3
Adam Alder <atariq11700@gmail.com>
2020-01-07 19:17:05 -0700

    ready to be tested, needs the adc function to get data

--- Drivers/uvDriver/uvDriver1.py
+++ Drivers/uvDriver/uvDriver1.py
@@ -1,17 +1,15 @@
+#adcData is a 12 bit binary string
+def uvSensor(self):
+#adc lsb found by Vrefin / 4096 which is 5V / 4096
+	voltageStep = 0.001220703125
 
-#data arg should be a 12bit binary number as type string
-def calculateIndex(data):
-    #adc lsb found by Vrefin / 4096 which is 5V / 4096
-    voltageStep = 0.001220703125
+#converts the 12 bit binary number to decimal
+#TODO Find how to get the adc data
+        numberOfSteps = int('''adcfunctioncall''', 2)
 
-    #converts the 12 bit binary number to decimal
-    numberOfSteps = int(data,2)
+#calculate the output voltage from UV sensor
+        voltage = numberOfSteps * voltageStep
 
-    #calculate the output voltage from UV sensor
-    voltage = numberOfSteps * voltageStep
-
-    #calculate the uvPower
-    #currentInNanoamps = Voltage / 4.3 * 1000
-    #uvPower = current / 113
-    uvPower = voltage * 1000 / 485.9
-    return(uvPower)
+#calculate the uvPower
+        uvPower = voltage * 1000 / 485.9
+        return uvPower
