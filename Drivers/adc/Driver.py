a604e95dddd63a78907a85292c438c83fa4f9cac
Paul Draper <my@email.org>
2020-01-14 19:57:17 -0700

    Fix bug, python does not allow overloaded functions :(

--- Drivers/adc/Driver.py
+++ Drivers/adc/Driver.py
@@ -11,29 +11,23 @@ class adc(Driver):
         """Constructor for the ADC driver
 
         Todo:
-            * Change the number of channels
+            * Change the number of numChannels
         """
         super().__init__("adc")
         self.SPI_PORT: int   = 0
         self.SPI_DEVICE: int = 0
-        self.channels: int   = 8
+        self.numChannels: int   = 8
         self.mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(self.SPI_PORT, self.SPI_DEVICE))
 
-    def read(self) -> List[int]:
+    def read(self, channel: int = -1) -> List[int]:
         """Read from the ADC
 
         Returns: 
-            list of int: the readings from each of the channels of the ADC
+            list of int: the readings from each of the numChannels of the ADC
         """
-        return [self.mcp.read_adc(i) for i in range(self.channels)]
 
-    def read(self, channel: int) -> int:
-        """Read from the ADC
+        if channel > -1:
+            return self.mcp.read_adc(channel)
+        else:
+            return [self.mcp.read_adc(i) for i in range(self.numChannels)]
 
-        Args:
-            channel (int): the channel to read from 
-
-        Returns: 
-            int: the reading from the specified channel
-        """
-        return self.mcp.read_adc(channel)
