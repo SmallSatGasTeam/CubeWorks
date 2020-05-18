4533d0e023d76f76111159317ce647be5928d2e2
CommanderScotty <scott.glaittli@gmail.com>
2020-05-18 14:43:30 -0600

    fixed merge conflict

--- runTests.py
+++ runTests.py
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import unittest
-
-from tests import unit_testing_example
-
-def run():
-    """
-    Defines a suite of unit tests from the tests module and runs them.  
-    """
-    suite = unittest.TestSuite()
-
-    tests = [unit_testing_example.DemoTest]
-
-    for test in tests:
-        suite.addTest(unittest.makeSuite(test))
-
-    runner = unittest.TextTestRunner(verbosity=2)
-    print(runner.run(suite))
-
-
-if __name__ == '__main__':
-    run()
-=======
 import unittest
 
 from tests import unit_testing_example, testCamera
@@ -42,4 +19,3 @@ def run():
 
 if __name__ == '__main__':
     run()
->>>>>>> 143542699d5e03c2eb520f08e62d1eda6122dc4f
