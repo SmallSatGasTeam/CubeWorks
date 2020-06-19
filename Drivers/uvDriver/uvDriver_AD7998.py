49b44f93d81a97eb3596a102be70f835d39f82a8
presten-stevens <presten.stevens@aggiemail.usu.edu>
2020-06-19 14:02:37 -0600

    fixing bugs

--- Drivers/uvDriver/uvDriver_AD7998.py
+++ Drivers/uvDriver/uvDriver_AD7998.py
@@ -1,2 +1,15 @@
+from Drivers.adc.ADC_DriverII import ADC
 
+
+class uvSensor(ADC):
+
+    adcChannel = 1
+
+    def __init__(self):
+        super().__init__("uvSensor")
+
+    def uvRead(self):
+        voltage = super().read(self.adcChannel)
+
+        return voltage
 
