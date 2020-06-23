96946b80dd5ee8f4e67378d53ac029229ea5b9b0
Benjamin Lewis <46497493+BenJayLew@users.noreply.github.com>
2020-06-23 16:43:39 -0600

    Create testADC.py

---
+++ tests/testADC.py
@@ -0,0 +1,26 @@
+import unittest
+import sys
+sys.path.append("../")
+
+from Drivers.adc import ADC_Driver
+
+class ADCTest(unittest.TestCase):
+    """
+    A test case containing sets of assertions in the form of class methods.
+    """
+    def testDivTwo(self):
+        """Asserts that 4 / 2 == 2.  Assertion should pass"""
+        self.assertEqual(divByTwo(4), 2)
+    def testtewo(self):
+        """Asserts that 4 / 2 == 3.  Assertion should fail"""
+        self.assertEqual(divByTwo(4), 3)
+    def testhree(self):
+        """Asserts that 6 / 2 == 3.  Assertion should pass"""
+        self.assertEqual(divByTwo(6), 3)
+    def testtree(self):
+        """Asserts that 'tree' / 2 == 3.  Assertion should result in an error"""
+        self.assertEqual(divByTwo('tree'), 3)
+
+    
+if __name__ == '__main__':
+    unittest.main()
