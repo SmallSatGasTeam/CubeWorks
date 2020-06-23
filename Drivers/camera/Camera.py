7d6386399409f1a12dd529a69bf56433a2023d09
Pi <pi@bluezone.com>
2020-06-23 12:39:06 -0600

    changes to camera driver

---
+++ Drivers/camera/Camera.py
@@ -0,0 +1,34 @@
+from Drivers.Driver import Driver
+from time import sleep
+from picamera import PiCamera
+
+class Camera(Driver):
+    def __init__(self):
+        """
+        Takes a picture
+        """
+        super().__init__("Camera")
+
+        self.highRes = (3280, 2464)
+        self.lowRes = (640, 480)
+
+    def takePicture(self):
+        """
+        Takes the picture
+        """
+        #the way to count folders in directory is len(os.listdir(path of directory to count in))
+        #you have to import OS
+        #This also counts files in the total, but with the file structure we came up with this shouldn't be a problem
+        numPictures = #count number of folders in directory
+        camera = PiCamera()
+        camera.resolution = self.lowRes
+        sleep(2)
+        camera.capture(#count+1/LowRes/'LowResOriginal.jpg')
+        camera.resolution = self.highRes
+        camera.capture(#count+1/HighRes/'HighResOriginal.jpg')
+
+    def compress(self):
+        """
+        Compresses the image using SSDV
+        """
+
