bfe5b43a1bb7b24f535388644ac5e2787d8b8f00
Pi <pi@bluezone.com>
2020-06-23 10:29:31 -0600

    Boom Deployer Driver

--- Drivers/boomDeployer/boom_deployer.py
+++ Drivers/boomDeployer/boom_deployer.py
@@ -8,8 +8,8 @@ class BoomDeployer(Driver):
         Calls parent constructor, Defines initial burn time, time to wait in between burns,
         and how many times to burn before giving up.  Sets up the GPIO pin for use by the actuate method.
         """
-        super().__init__("Camera")
-        # Initial values 
+        super().__init__("BoomDeployer")
+        # Initial values
         self.burnTime = 4
         self.waitTime = 10
         self.numTimes = 3
@@ -17,25 +17,57 @@ class BoomDeployer(Driver):
         # Set up the GPIO 8 for use
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(8, GPIO.OUT, initial=GPIO.HIGH)
+
+	#First Wirecutter
+	self.wireCutter1_high1 = 36
+	self.wireCutter1_high2 = 38
+	self.wireCutter1_low1 = 7
+        GPIO.setup(self.wireCutter1_high1, GPIO.OUT, initial=GPIO.LOW)
+	GPIO.setup(self.wireCutter1_high2,GPIO.OUT, initial=GPIO.LOW)
+	GPIO.setup(self.wireCutter1_low1,GPIO.OUT, initial=GPIO.HIGH)
+
+	#Second Wirecutter
+	self.wireCutter2_high1 = 35
+	self.wireCutter2_high2 = 37
+	self.wireCutter2_low1 = 29
+        GPIO.setup(self.wireCutter2_high1, GPIO.OUT, initial=GPIO.LOW)
+	GPIO.setup(self.wireCutter2_high2,GPIO.OUT, initial=GPIO.LOW)
+	GPIO.setup(self.wireCutter2_low1,GPIO.OUT, initial=GPIO.HIGH)
 
 
     def deploy(self):
         """
-        Loop a specified number of times, turning off the GPIO pin to start the burn, 
-        Wait for a specified amount of time, turn on the pin to end the burn, 
-        wait again, and loop, increasing the burn time on each iteration
+        Loop a specified number of times, setting the correct GPIO pins to HIGH/LOW  to start/stop
+	the burn. Wait and then repeat with the other wirecutter mechanism
         """
         for num in range(0, self.numTimes):
-            # Turn off begin burn
-            GPIO.output(8, GPIO.LOW) 
-            # Burn for burnTime + num seconds
-            sleep(self.burnTime + num) 
-            # Turn on end burn
-            GPIO.output(8, GPIO.HIGH)
-            # wait
-            sleep(self.waitTime) 
+	    #Turn on Wire Cutter 1
+            GPIO.output(self.wireCutter1_high1, GPIO.HIGH)
+            GPIO.output(self.wireCutter1_high2, GPIO.HIGH)
+            GPIO.output(self.wireCutter1_low1, GPIO.LOW)
+	    #Burn for set number of seconds
+	    sleep(self.burnTime)
+	    #Turn off Wire Cutter 1
+	    GPIO.output(self.wireCutter1_high1, GPIO.LOW)
+            GPIO.output(self.wireCutter1_high2, GPIO.LOW)
+            GPIO.output(self.wireCutter1_low1, GPIO.HIGH)
+	    #Wait
+	    sleep(self.waitTime)
 
+
+	    #Turn on Wire Cutter 2
+            GPIO.output(self.wireCutter2_high1, GPIO.HIGH)
+            GPIO.output(self.wireCutter2_high2, GPIO.HIGH)
+            GPIO.output(self.wireCutter2_low1, GPIO.LOW)
+	    #Burn for set number of seconds
+	    sleep(self.burnTime)
+	    #Turn off Wire Cutter 2
+	    GPIO.output(self.wireCutter2_high1, GPIO.LOW)
+            GPIO.output(self.wireCutter2_high2, GPIO.LOW)
+            GPIO.output(self.wireCutter2_low1, GPIO.HIGH)
+	    #Wait
+	    sleep(self.waitTime)
+	GPIO.cleanup()
     def read(self):
         """
         Left undefined as no data is collected by this component
