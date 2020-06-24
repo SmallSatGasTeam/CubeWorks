f1ae64cf5951a59517a0f736a182778af8cd1714
Benjamin Lewis <46497493+BenJayLew@users.noreply.github.com>
2020-06-23 21:21:55 -0600

    Update testUV.py

--- tests/testUV.py
+++ tests/testUV.py
@@ -1,25 +1,8 @@
-import unittest
+import sys
+sys.path.append("../")
 
-import Drivers.uvDriver.uvDriver_AD7998 as UVOne
-import Drivers.uvDriver.uvDriver_mcp3008 as UVTwo
+from Drivers.UV import UVDriver
 
-class TestUV(unittest.TestCase):
-    """
-    Initializes a test case for the UV Sensors.  Inherits from unittest.TestCase.
-    Defines methods that test the following:
-    1. The AD7998 input.
-    2. The MCP3008 input.
-    """
-
-    testUVOne = UVOne.uvSensor()
-
-    def testADPower(self):
-        # voltage: 4v
-        self.assertEqual(self.testUVOne, True)
-        # voltage: 3.3v
-        self.assertAlmostEqual(self.testUVOne, 6.7915208)
-        # voltage: 0v
-        self.assertAlmostEqual(self.testUVOne, 0.0000000)
-        # voltage: 2v
-        self.assertAlmostEqual(self.testUVOne, 4.1160732)
-
+    
+uv = UVDriver()
+print(uv.read)
