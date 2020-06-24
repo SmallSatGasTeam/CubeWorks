82827e85ef9a79657f4c520cd090396a7d38379a
loganfreeman1 <67300766+loganfreeman1@users.noreply.github.com>
2020-06-24 13:25:30 -0600

    Update testEPS.py

--- tests/testEPS.py
+++ tests/testEPS.py
@@ -1,1 +1,19 @@
-test
+import sys
+sys.path.append('../')
+from Drivers.eps import EPS
+from time import sleep()
+
+epsTest = EPS()
+
+#Test battery raw disable/enable
+epsTest.enableRaw()
+print("Raw Battery Voltage Enabled for 10 seconds")
+sleep(10)
+epsTest.disableRaw()
+print("Raw Battery Voltage Disabled for 10 seconds")
+sleep(10)
+epsTest.enableRaw()
+print("Raw Battery Voltage Re-enabled")
+
+#Test all read functions
+print("MCU Temp in C: "+str(epsTest.getMCUTemp()))
