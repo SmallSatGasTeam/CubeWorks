d271c0ed93e1658a0d1aca6b35294df5cfcc79c9
Benjamin Lewis <46497493+BenJayLew@users.noreply.github.com>
2020-06-24 12:33:36 -0600

    Update solarDriver.py

--- Drivers/solarPanelTemp/solarDriver.py
+++ Drivers/solarPanelTemp/solarDriver.py
@@ -23,13 +23,13 @@ class TempSensor(Driver):
         """
         Sends a read command with a specified channel and then returns the reply from the ADC
         """
-        temp0_raw = spi0.readbytes(2)  #We need just the first 13 bits, as the last three bits of the 16 bit(2 byte) word are not used
+        temp0_raw = self.spi0.readbytes(2)  #We need just the first 13 bits, as the last three bits of the 16 bit(2 byte) word are not used
 
         #This function converts the two bytes to the temperature value. The data is stored as two bytes where T13 is the most significant bit
         #| T13 T12 T11 T10 T9 T8 T7 T6 |  | T5 T4 T3 T2 T1 1 1 1 |
         temp0 = ((temp0_raw[0] * 64) + (temp0_raw[1] >> 3))* 0.0625
         
-        temp1_raw = spi1.readbytes(2)  #We need just the first 13 bits, as the last three bits of the 16 bit(2 byte) word are not used
+        temp1_raw = self.spi1.readbytes(2)  #We need just the first 13 bits, as the last three bits of the 16 bit(2 byte) word are not used
 
         #This function converts the two bytes to the temperature value. The data is stored as two bytes where T13 is the most significant bit
         #| T13 T12 T11 T10 T9 T8 T7 T6 |  | T5 T4 T3 T2 T1 1 1 1 |
