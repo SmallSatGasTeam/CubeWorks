f6d14753e2bd5ed53f88052834adfacca76f46d7
loganfreeman1 <67300766+loganfreeman1@users.noreply.github.com>
2020-06-24 12:49:55 -0600

    Update AntennaDoor.py

--- Drivers/antennaDoor/AntennaDoor.py
+++ Drivers/antennaDoor/AntennaDoor.py
@@ -1,1 +1,19 @@
-faksdf
+from Drivers.Driver import Driver
+import smbus
+
+class AntennaDoor(Driver):
+
+    def __init__(self):
+        super().__init__("AntennaDoor") #Calls parent constructor
+
+        #Setup I2C bus for communication
+        self.__DEVICE_BUS = 1
+        self.__DEVICE_ADDR = 0x33
+        self.__RegisterADR = 0x00 # I do not know what this means or what it should be
+        self.__bus = smbus.SMBus(DEVICE_BUS)
+      
+    def readDoorStatus():
+      #returns the status of all 4 antenna doors
+      doorStatus = bus.read_i2c_block_data(self.__DEVICE_ADDR, self.__RegisterADR, 1)
+      return doorStatus
+
