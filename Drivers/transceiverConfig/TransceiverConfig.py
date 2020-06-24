b8e53f3ebc96a0bdeaaf9d3aba9859cf5df52367
Jack Danos <jackadanos@gmail.com>
2020-06-24 11:53:33 -0600

    Update TransceiverConfig.py

--- Drivers/transceiverConfig/TransceiverConfig.py
+++ Drivers/transceiverConfig/TransceiverConfig.py
@@ -6,38 +6,49 @@ class TransceiverConfig(Driver):
     """
     The purpose of this driver is to modify the configuration of the Endurosat UHF Transceiver. 
     It does not have anything to do with sending packets over the radio.
-    The only configuration option that our flight logic requires is the ability to turn on and off morse code beacons.
+    We need the ability to turn on and off the beacon, turn on low power mode, and read the internal temp sensor.
+    See Page 25 of the Endurosat UHF Transceiver Type II Manual Rev 1.8 document for the SCW bit description.
+    
+    ***Note: ALL ES+ commands need to be changed to reflect the address of the transceiver - currently set to 23***
+    ***Potentially need to add CRC32 checksum functionality***
     """
     super().__init__("TransceiverConfig")
     
-  def setBeaconOn(self):
+  def writeData(self, input):
     """
-    Turns on the morse beacon
+    Writes the input to the transceiver over UART
     """
     ser = serial.Serial ("/dev/ttyAMA0")               #Open named port 
     ser.baudrate = 115200                              #Set baud rate to 9600
-    data = "ES+W23003321"                              #Set data to the character 'a', 0x61 or 01100001
+    data = input                              #Set data to the character 'a', 0x61 or 01100001
     ser.write(data.encode()) #Send the data
-    while True:
-              sleep(.15)
-              data = "Hello there"
-              ser.write(data.encode()) #Send the data
-
     ser.close()
+    
+  def setBeaconOn(self):
+    """
+    Turns on the morse beacon. Leaves all values at default except beacon.
+    Binary SCW: 11001101000001
+    Hex SCW: 3341
+    """
+    writeData("ES+W23003341")
   
   def setBeaconOff(self):
     """
-    Turns off the morse beacon
+    Turns off the morse beacon. All values are back to default.
+    Binary SCW: 11001100000001
+    Hex SCW: 3301
     """
-    
+    writeData("ES+W23003301")
+
   def setLowPowerMode(self):
     """
     Turns on Low Power Mode. Note: Any ESTTC command can be used to bring the transceiver out of low power mode
     """
-    
-    
+    writeData("ES+W23F4")
+
   def read(self):
     """
-    We don't need any data from the transceiver so this returns nothing
+    Returns the temperature from the transceiver internal temp sensor
     """
-    pass
+    writeData("ES+R230A")
+    return temp
