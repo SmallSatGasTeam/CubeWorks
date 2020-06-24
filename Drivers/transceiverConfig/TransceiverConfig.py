dd8613b9215bfaf7d49f5c057be3cab4ed660478
Jack Danos <jackadanos@gmail.com>
2020-06-24 10:19:06 -0600

    Update TransceiverConfig.py

--- Drivers/transceiverConfig/TransceiverConfig.py
+++ Drivers/transceiverConfig/TransceiverConfig.py
@@ -1,4 +1,5 @@
 from Drivers.Driver import Driver
+import serial
 
 class TransceiverConfig(Driver):
   def __init__(self):
@@ -13,12 +14,22 @@ class TransceiverConfig(Driver):
     """
     Turns on the morse beacon
     """
-    
+    ser = serial.Serial ("/dev/ttyAMA0")               #Open named port 
+    ser.baudrate = 115200                              #Set baud rate to 9600
+    data = "ES+W23003321"                              #Set data to the character 'a', 0x61 or 01100001
+    ser.write(data)                                    #Send the data
+    ser.close()
   
   def setBeaconOff(self):
     """
     Turns off the morse beacon
     """
+    
+  def setLowPowerMode(self):
+    """
+    Turns on Low Power Mode. Note: Any ESTTC command can be used to bring the transceiver out of low power mode
+    """
+    
     
   def read(self):
     """
