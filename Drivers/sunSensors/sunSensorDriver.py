54950488e273f308e4094fb4866df6d44a45a07c
Benjamin Lewis <46497493+BenJayLew@users.noreply.github.com>
2020-06-23 22:09:45 -0600

    Update sunSensorDriver.py

--- Drivers/sunSensors/sunSensorDriver.py
+++ Drivers/sunSensors/sunSensorDriver.py
@@ -1,23 +1,6 @@
 from Drivers.Driver import Driver
 from Drivers.adc import ADC_Driver
 
-class UVDriver(Driver):
-  """
-  This class calls the ADC driver and asks for data from the UV channel
-  """
-  adc = ADC_Driver.ADC()
-  uv_channel = 1  #The channel on the ADC that th UV sensor is connected to
-  
-  def __init__(self):
-    super().__init__("UVDriver")
-  
-  def read(self):
-    """
-    This function calls the read function of the ADC with the channel for the uv sensor
-    """
-    return self.adc.read(self.uv_channel)
-
-
 class sunSensor(Driver):
     """
     This class calls the ADC driver and asks for data from the UV channel
