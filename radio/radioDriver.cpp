820b5a86bce10aec2c28cb4a8c2664c21cb08221
dstevensaggie <dallin.stevens@aggiemail.usu.edu>
2020-06-23 20:16:44 -0600

    Adding plan for the class as presented in Software Team Meeting

--- radio/radioDriver.cpp
+++ radio/radioDriver.cpp
@@ -22,11 +22,15 @@ std::vector<std::vector<int>> RadioDriver::receive() {
 	std::vector<int> testInner;
 	testOuter.push_back(testInner);
 	return testOuter;
+
+
+	// GET THE DATA FROM PRESTEN AND THEN CALL QUERY DB WHICH THEN SENDS TO PACKETIZE
+	// MAKE THIS FUNCTION TTHE DRIVER?? OR DO IT IN THE MAIN FUNCTION???
 }
 
 void RadioDriver::queryDB(bool pic, std::string query) {
 
-	// if pic is true, get picture data.  else query database and put in unPacetizedData
+   // if pic is true, get picture data.  else query database and put in unPacetizedData
 	if (pic) {
 		// run code to gather pic data from the database
 	}
@@ -53,7 +57,29 @@ void RadioDriver::queryDB(bool pic, std::string query) {
 }
 
 void RadioDriver::packetize() {
+
+			// NOTES FROM SHAWN:
+	// THIS WILL BE RECIEVED FROM A python
+	// check 1: power check eps driver
+	// check 2: decode the message. what type if picture what number of picture?
+	// check 3: Check and set transmission windows 
+	// check 4: How many bytes are we going to transmit 
+
+	// datatype, transmission windows, bytes will be passed in as command line arguments.
+	// call delay function to delay until we hit trasmission window. 5 seconds before call shawn's function
+	// pass to shawn datatype and how many lines we are trasmitting from the file. 
+	// Communicate with shawn the exact file path and the file name. 
+
+	// CREATE THIS CLASS AS AN EXECUTABLE THAT ACCEPTS THE REQUIRED PARAMETERS AS COMMAND LINE ARGUMENTS
+	// GET FROM SHAWN THE NAME OF HIS EXECUTABLE AND CALL IT IN THIS CLASS PASSING THE REQUIRED PARAMETERS AS COMMNAD LINE ARGUMENTS
+
+    /////////////////////////////////////////////////////////////////
+
 	// packetize m_unPacketizedData an put it in m_packets
+    // move from std::vector<std::vector<std::string>> to the class. 
+
+
+	// send 128 bytes to each line of the transmit file. Count how many lines and store that in a header. Erase old file before writing.
 }
 
 void RadioDriver::packetizePic() {
