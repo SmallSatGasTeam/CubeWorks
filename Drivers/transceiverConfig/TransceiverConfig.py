c717ec70589b7a014df2bc9f78162b3450dfda6f
Jack Danos <jackadanos@gmail.com>
2020-06-24 10:05:03 -0600

    Update TransceiverConfig.py

--- Drivers/transceiverConfig/TransceiverConfig.py
+++ Drivers/transceiverConfig/TransceiverConfig.py
@@ -7,4 +7,21 @@ class TransceiverConfig(Driver):
     It does not have anything to do with sending packets over the radio.
     The only configuration option that our flight logic requires is the ability to turn on and off morse code beacons.
     """
+    super().__init__("TransceiverConfig")
     
+  def setBeaconOn(self):
+    """
+    Turns on the morse beacon
+    """
+    
+  
+  def setBeaconOff(self):
+    """
+    Turns off the morse beacon
+    """
+    
+  def read(self):
+    """
+    We don't need any data from the transceiver so this returns nothing
+    """
+    pass
