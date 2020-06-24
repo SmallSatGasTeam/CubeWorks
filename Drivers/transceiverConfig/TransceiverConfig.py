59867c5bfebc9e127c9434463f6ff42ffc3de494
Jack Danos <jackadanos@gmail.com>
2020-06-24 09:53:37 -0600

    Update TransceiverConfig.py

--- Drivers/transceiverConfig/TransceiverConfig.py
+++ Drivers/transceiverConfig/TransceiverConfig.py
@@ -1,1 +1,10 @@
-from Drivers import Driver
+from Drivers.Driver import Driver
+
+class TransceiverConfig(Driver):
+  def __init__(self):
+    """
+    The purpose of this driver is to modify the configuration of the Endurosat UHF Transceiver. 
+    It does not have anything to do with sending packets over the radio.
+    The only configuration option that our flight logic requires is the ability to turn on and off morse code beacons.
+    """
+    
