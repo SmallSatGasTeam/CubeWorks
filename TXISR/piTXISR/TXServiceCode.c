8aae4ad864687ac3c32184f951875919298ce830
ShawnBraden <sbradenjones16@gmail.com>
2020-06-24 21:18:34 -0600

    started the basics of the code

--- TXISR/piTXISR/TXServiceCode.c
+++ TXISR/piTXISR/TXServiceCode.c
@@ -3,14 +3,36 @@
 #include <termios.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 #define ENABLE "conifPinTXISR"
 #define DISABLE "conifPinTXISRDone"
+#define FLAG_FILE "./out.txt" //change this later for the real program
 
-void main(void)
+
+void main(int argc,char* argv[])
 {
-    printf("echo Hello\n");
-    int ret = system("echo hello world");
-    if(ret == 0) printf("It worked\n");
-    else if (ret == -1) printf("didn't work\n");
-    printf("The value of ret: %d\n", ret);
+    int numOfRecords = argv[2];
+    int dataType = argv[3];
+
+    //open the transmition file
+    FILE *txFile;
+    if ((txFile = fopen(argv[1],"r")))
+    {
+        //if we fail reboot
+        fclose(txFile);
+        exit(1);
+    }
+
+    //config linuxs to give us the pins
+    int enable = system(ENABLE);
+    //if we fail reboot
+    if(enable == -1) exit(1); 
+
+
+
+    //debuging
+    // int ret = system("echo hello world");
+    // if(ret == 0) printf("It worked\n");
+    // else if (ret == -1) printf("didn't work\n");
+    // printf("The value of ret: %d\n", ret);
 }
