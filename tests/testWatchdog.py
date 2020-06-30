7efbbeef326cb82656e1706353b6c35a39ae6c3a
Jack Danos <jackadanos@gmail.com>
2020-06-30 15:04:03 -0600

    Update testWatchdog.py

--- tests/testWatchdog.py
+++ tests/testWatchdog.py
@@ -2,8 +2,8 @@
 This code is designed to test the Arduino Beetle Watchdog
 """
 import sys
-sys.path.append('../')
-from watchdog.Heartbeat import longTap
+sys.path.append('../watchdog/Heartbeat')
+from Heartbeat import longTap
 
 # Run the heartbeat code that sends a pulse every 4 seconds
 Heartbeat.longTap()
