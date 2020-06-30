2cf75d493de6873a9f81a4a99d2933e7e569ba6a
Jack Danos <jackadanos@gmail.com>
2020-06-30 15:09:31 -0600

    Update testWatchdog.py

--- tests/testWatchdog.py
+++ tests/testWatchdog.py
@@ -6,4 +6,6 @@ sys.path.append('../watchdog/Heartbeat')
 from Heartbeat import longTap
 
 # Run the heartbeat code that sends a pulse every 4 seconds
+print("Sending the heartbeat pulse every 4 seconds")
 longTap()
+print("Test")
