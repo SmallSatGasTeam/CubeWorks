d26fd1cf51df99eced30f871ee6d768f37eeb9d8
DallinStevens <65097494+dkstevensaggie@users.noreply.github.com>
2020-07-13 22:12:43 -0600

    Update TXServiceCode.c

--- TXISR/piTXISR/TXServiceCode.c
+++ TXISR/piTXISR/TXServiceCode.c
@@ -17,7 +17,7 @@
 
 #define FLAG_FILE "./out.txt" //change this later for the real program
 #define FORMAT_FILE "./temp.txt" //this is the file that dallan will creat
-#define UART_PORT "/dev/serial0" //this is serial port name, make sure this is correct for the final code
+#define UART_PORT "/dev/ttyS0" //this is serial port name, make sure this is correct for the final code
 
 //this is our time delay
 #define DELAY_tx 120
