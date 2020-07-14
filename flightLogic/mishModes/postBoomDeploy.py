ffd1c84418b76da8ef086ccb3ae4f5ec2c67dfab
loganfreeman1 <67300766+loganfreeman1@users.noreply.github.com>
2020-07-14 14:46:50 -0600

    finish post boom mode

--- flightLogic/mishModes/postBoomDeploy.py
+++ flightLogic/mishModes/postBoomDeploy.py
@@ -5,6 +5,23 @@ import Drivers.eps.EPS as EPS
 
 class postBoomMode:
 	def __init__(self):
-	
-	async def run
+		postBoomTimeFile = f.open("postBoomTime.txt", "w+")
+	async def run(self):
+		#Set up background processes
+		ttncData = TTNCData()
+		attitudeData = AttitudeData()
+		asyncio.run(ttncData.collectTTNCData(), attitudeData.collectAttitudeData())
+		safeMode = safe()
+		asyncio.run(safeMode.thresholdCheck())
 		
+		await while True:
+			if not postBoomTimeFile.read(1):
+				postBoomTimeFile.write(str(0))#File is empty
+			else:
+				runningTime = int(postBoomTimeFile.read())
+				if runningTime>86400: #Live for more than 24 hours
+					postBoomTimeFile.write(str(0))
+					postBoomTimeFile.close()
+				else: 
+					postBoomTimeFile.write(str(runningTime+60))
+			await asyncio.sleep(60) #check every 60 seconds if post boom has been running for more than 24 hours
