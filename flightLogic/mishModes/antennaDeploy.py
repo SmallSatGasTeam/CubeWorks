65a1fd236df884f29f4393f5736d87a12d26e7db
loganfreeman1 <67300766+loganfreeman1@users.noreply.github.com>
2020-07-14 11:49:51 -0600

    Update antennaDeploy.py

--- flightLogic/mishModes/antennaDeploy.py
+++ flightLogic/mishModes/antennaDeploy.py
@@ -21,16 +21,16 @@ class antennaMode:
         attitudeData = AttitudeData()
 	asyncio.run(ttncData.collectTTNCData(), attitudeData.collectAttitudeData())
         safeMode = safe()
-            
+	eps = EPS() #creating EPS object    
         #Check battery conditions - changed from jack's flight logic based on returning structure - will return to 
-		await while True:
-			epsVoltage = EPS.getBusVoltage()
-			if epsVoltage < self.criticalVoltage:
-                		safeMode.run(10) #1 hour
-			await asyncio.sleep(1) #check voltage every second
+	await while True:
+		epsVoltage = eps.getBusVoltage()
+		if epsVoltage < self.criticalVoltage:
+			safeMode.run(10) #1 hour
+		await asyncio.sleep(1) #check voltage every second
             
         #Perform tasks for antenna deployment
-        eps = EPS() #creating EPS object
+
         
         await while True:
             if (eps.getBusVoltage()>self.thresholdVoltage):
