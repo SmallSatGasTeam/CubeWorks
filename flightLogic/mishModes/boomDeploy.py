9692921dec0ef7fafec1902be8623307466bad6a
loganfreeman1 <67300766+loganfreeman1@users.noreply.github.com>
2020-07-14 14:29:26 -0600

    Finish boom deploy mode

--- flightLogic/mishModes/boomDeploy.py
+++ flightLogic/mishModes/boomDeploy.py
@@ -1,6 +1,27 @@
 import asyncio
 from safe import safe
 from getDriverData import *
-import Drivers.eps.EPS as EPS
+import Drivers.boomDeployer.BoomDeployer
+import Drivers.camera.Camera
 
-class BoomDeploy
+class boomMode:
+	def __init__(self):
+		None
+	def run(self):
+		#Setting up background processes
+		ttncData = TTNCData()
+		attitudeData = AttitudeData()
+		deployData = DeployData()
+		asyncio.run(ttncData.collectTTNCData(), attitudeData.collectAttitudeData(), deployData.collectDeployData())
+		safeMode = safe()
+		asyncio.run(safeMode.thresholdCheck())
+		
+		#Deploy boom, take picture
+		deployer = BoomDeployer()
+		cam = Camera()
+		deployer.deploy()
+		cam.takePicture()
+		cam.compressLowResToFiles()
+		cam.compressHighResToFiles()
+		return True #Go to post-boom deploy
+		
