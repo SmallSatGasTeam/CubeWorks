6f55f6cb3e820468fe98585449002868956345a9
loganfreeman1 <67300766+loganfreeman1@users.noreply.github.com>
2020-07-15 17:38:53 -0600

    Update preBoomDeploy.py

--- flightLogic/missionModes/preBoomDeploy.py
+++ flightLogic/missionModes/preBoomDeploy.py
@@ -6,7 +6,7 @@ import Drivers.eps.EPS as EPS
 class preBoomMode:
 	def __init__(self):
 		self.thresholdVoltage = 5 #Threshold voltage to deploy AeroBoom. 
-        self.criticalVoltage = 3.3 #Critical voltage, below this go to SAFE
+        	self.criticalVoltage = 3.3 #Critical voltage, below this go to SAFE
 		self.darkVoltage = 1 #Average voltage from solar panels that, if below this, indicates GASPACS is in darkness
 		self.darkMinutes = 5 #How many minutes GASPACS must be on the dark side for before moving forward
 		self.lightMinimumMinutes = 5 #Minimum amount of time GASPACS must be on light side of orbit before deploying
@@ -16,9 +16,9 @@ class preBoomMode:
 		self.timeWaited = 0
 		
 	async def run(self):
-		ttncData = TTNCData()
+	ttncData = TTNCData()
         attitudeData = AttitudeData()
-		asyncio.run(ttncData.collectTTNCData(), collectAttitudeData())
+	asyncio.run(ttncData.collectTTNCData(2), collectAttitudeData())#Pre-boom is mode 2
         safeMode = safe()
 		asyncio.run(safeMode.thresholdCheck()) #Check battery conditions, run safe mode if battery drops below safe level 
 		eps = EPS() #creating EPS object  
