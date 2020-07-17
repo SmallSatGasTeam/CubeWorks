ba0e118f42cfa42eef04905511727d6cd3027b04
loganfreeman1 <67300766+loganfreeman1@users.noreply.github.com>
2020-07-17 15:18:33 -0600

    Update asyncioTest.py

--- flightLogic/asyncioTest.py
+++ flightLogic/asyncioTest.py
@@ -1,16 +1,15 @@
 import asyncio
 import time
 import random
-from contextlib import suppress
 
-async def main():
-	tMode = testMissionMode()
-	task = await asyncio.gather(tMode.run())
+async def main(): #Imitates main flight logic function, run in console with asyncio.run(main())
+	tMode = testMissionMode() 
+	task = await asyncio.gather(tMode.run()) #This is how mission modes will need to be run, probably
 	print("tMode.run() should be finished now. Waiting 5 seconds then exiting")
-	await asyncio.sleep(5)
+	await asyncio.sleep(5) #This is how sleep will be done in the main function
 	return
 
-class testClassOne:
+class testClassOne: #imitates a data-getter
 	def __init__(self):
 		None
 
@@ -19,7 +18,7 @@ class testClassOne:
 			print("printOnes from testClassOne running, printing every second")
 			await asyncio.sleep(1)
 
-class testClassTwo:
+class testClassTwo: #Another data-getter
 	def __init__(self):
 		None
 
@@ -28,23 +27,23 @@ class testClassTwo:
 			print("printTwos from testClassTwo running, printing every 2 seconds")
 			await asyncio.sleep(2)
 
-class testMissionMode:
+class testMissionMode: #imitates a mission mode
 	def __init__(self):
-		self.number = [0]
+		self.number = [0] #Have to use a list to pass information back from asynchronous methods, since integers/booleans are immutable
 
-	async def run(self):
+	async def run(self): #Run function of mission mode
 		tOne = testClassOne()
 		tTwo = testClassTwo()
-		tasks = [asyncio.create_task(tTwo.printTwos()),asyncio.create_task(tOne.printOnes()),asyncio.create_task(self.getNumber(self.number)),asyncio.create_task(self.setNumber(self.number))]
+		tasks = [asyncio.create_task(tTwo.printTwos()),asyncio.create_task(tOne.printOnes()),asyncio.create_task(self.getNumber(self.number)),asyncio.create_task(self.setNumber(self.number))] #This is how background tasks must be set up
 		await asyncio.sleep(10) #Function runs for 10 seconds
-		try:
+		try: #Probably stop tasks in mission modes, do it like this
 			for t in tasks:
 				t.cancel()
 		except asyncio.exceptions.CancelledException:
 			print("exception in canceling in testMissionMode.run()")
 		return
 
-	async def setNumber(self, num):
+	async def setNumber(self, num): #These two methods imitate somethign like the battery checker reporting whether battery level is good for deployment
 		while True:
 			num[0] = random.randint(1,101)
 			print("number set as " + str(num[0]))
