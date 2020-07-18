d0ac41df634351ac78e2715f5a374e82f3991ee7
loganfreeman1 <67300766+loganfreeman1@users.noreply.github.com>
2020-07-18 10:27:29 -0600

    Update antennaDeploy.py

--- flightLogic/missionModes/antennaDeploy.py
+++ flightLogic/missionModes/antennaDeploy.py
@@ -15,28 +15,37 @@ class antennaMode:
         self.timeWaited = 0 #Time already waited - zero
         self.__getDataTTNC = getDriverData.TTNCData(saveobject)
 	self.__getDataAttitude = getDriverData.AttitudeData(saveobject)
+	self.__tasks = [] #List will be populated with background tasks to cancel them
 
     async def run(self):
         ttncData = self.__getDataTTNC
         attitudeData = self.__getDataAttitude
-        asyncio.run(ttncData.collectTTNCData(1))# NOTE: Having both the following asyncio.run statements in one statement caused error
+        self.__tasks.append(asyncio.create_task(ttncData.collectTTNCData(1)) #asyncio.run is exclusive, 
         asyncio.run(attitudeData.collectAttitudeData()) #Antenna Deploy is mission mode 1
         safeMode = safe()
 
 	asyncio.run(safeMode.thresholdCheck()) #Check battery conditions, run safe mode if battery drops below safe level 
-	eps = EPS() #creating EPS object    
 
-        #Perform tasks for antenna deployment
-        await while True:
-            if (eps.getBusVoltage()>self.deployVoltage):
-                antennaDeploy.deploy()
-                if doorStatus == (0,0,0,0): #probably need to change this to actually work
-                    #Doors are open
-                    return True
-            else:
-                if(self.timeWaited > self.maximumWaitTime):
-                    safeMode.run(10) #1 hour
-                else:
-                    #Wait 1 minute
-                    self.timeWaited = self.timeWaited+1
-                    await asyncio.sleep(60)
+	async def deployAntenna(self):
+		#Perform tasks for antenna deployment
+		eps=EPS()
+        	await while True:
+	 		if (eps.getBusVoltage()>self.deployVoltage):
+                	antennaDeploy.deploy()
+                	if doorStatus == (0,0,0,0): #probably need to change this to actually work
+                    		#Doors are open, cancel all tasks and then 
+                    		return True
+            		else:
+                		if(self.timeWaited > self.maximumWaitTime):
+                    			safeMode.run(10) #1 hour
+                		else:
+                    			#Wait 1 minute
+                    			self.timeWaited = self.timeWaited+1
+                    			await asyncio.sleep(60)
+	
+	def cancellAllTasks(self, taskList):
+		try:
+			for t in taskList:
+				t.cancel()
+		except asyncio.exceptions.CancelledException:
+			print("Caught thrown exception in cancelling background task")
