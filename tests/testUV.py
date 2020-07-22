9f6352ff76f7693e0c3cb20adcb5d835a816e713
Benjamin Lewis <46497493+BenJayLew@users.noreply.github.com>
2020-07-22 15:16:21 -0600

    Update testUV.py

--- tests/testUV.py
+++ tests/testUV.py
@@ -10,10 +10,10 @@ class UVTest(unittest.TestCase):
     """
     def uvDark(self):
         """Asserts that the UV sensor reads < 0.2 volts when dark.  Assertion should pass when in dark"""
-        self.assertTrue(adc.read(1) <= 0.2, "This should be true if dark")
+        self.assertTrue(uv.read() <= 0.2, "This should be true if dark")
     def uvDark(self):
         """Asserts that the UV sensor reads > 3 volts when in uv light.  Assertion should pass when in uv light"""
-        self.assertTrue(adc.read(1) >= 3, "This should be true if in uv light")
+        self.assertTrue(uv.read() >= 3, "This should be true if in uv light")
 
     
 uv = UVDriver.UVDriver()
