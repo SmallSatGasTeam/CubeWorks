419fa6f75b25065654aa7cd20ccb47c2b2f584bf
Benjamin Lewis <46497493+BenJayLew@users.noreply.github.com>
2020-07-22 14:39:32 -0600

    Fixed test case names

--- tests/testADC.py
+++ tests/testADC.py
@@ -28,22 +28,22 @@ class ADCTest(unittest.TestCase):
         """Asserts that sun sensor 5 reads < 0.2 volts when dark.  Assertion should pass when in dark"""
         self.assertTrue(adc.read(0) <= 0.2, "This should be true if dark")
         
-    def uvDark(self):
+    def uvLight(self):
         """Asserts that the UV sensor reads > 3 volts when in uv light.  Assertion should pass when in uv light"""
         self.assertTrue(adc.read(1) >= 3, "This should be true if in uv light")
-    def sun1Dark(self):
+    def sun1Light(self):
         """Asserts that sun sensor 1 reads > 1 volts when in uv light.  Assertion should pass when in uv light"""
         self.assertTrue(adc.read(5) >= 1, "This should be true if in uv light")
-    def sun2Dark(self):
+    def sun2Light(self):
         """Asserts that sun sensor 2 reads > 1 volts when in uv light.  Assertion should pass when in uv light"""
         self.assertTrue(adc.read(4) >= 1, "This should be true if in uv light")
-    def sun3Dark(self):
+    def sun3Light(self):
         """Asserts that sun sensor 3 reads > 1 volts when in uv light.  Assertion should pass when in uv light"""
         self.assertTrue(adc.read(2) >= 1, "This should be true if in uv light")
-    def sun4Dark(self):
+    def sun4Light(self):
         """Asserts that sun sensor 4 reads > 1 volts when in uv light.  Assertion should pass when in uv light"""
         self.assertTrue(adc.read(3) >= 1, "This should be true if in uv light")
-    def sun5Dark(self):
+    def sun5Light(self):
         """Asserts that sun sensor 5 reads > 1 volts when in uv light.  Assertion should pass when in uv light"""
         self.assertTrue(adc.read(0) >= 1, "This should be true if in uv light")
     
