4828b936acb4e854fe5df701da470421ba71fdd3
ShawnBraden <sbradenjones16@gmail.com>
2020-07-28 22:34:17 -0600

    found the problem

--- TXISR/piTXISR/TXServiceCode.c
+++ TXISR/piTXISR/TXServiceCode.c
@@ -182,7 +182,7 @@ void main(int argc,char* argv[])
                 timeStamp[charTimeCount++] = ch;
                 PRINT_DEBUG_c(ch)
             }
-            else if (ch == ':')
+            if (ch == ':')
             {
                 end = 1;
                 DEBUG_P(END)
