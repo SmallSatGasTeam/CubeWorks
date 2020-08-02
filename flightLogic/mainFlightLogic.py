fe43f35c5a93fa1aad72b951a1185f83d45de1f6
loganfreeman1 <67300766+loganfreeman1@users.noreply.github.com>
2020-08-02 14:41:19 -0600

    Update mainFlightLogic.py

--- flightLogic/mainFlightLogic.py
+++ flightLogic/mainFlightLogic.py
@@ -1,12 +1,17 @@
 # This is the main file, to be run on startup of the Pi
-import getDriverData
-import saveTofiles
-import Drivers.antennaDoor.AntennaDoor as antennaDoor
-import missionModes.antennaDeploy
-import missionModes.preBoomDeploy
-import missionModes.boomDeploy
-import missionModes.postBoomDeploy
+import sys
+sys.path.append('../')
+import os.path
+from flightLogic import getDriverData
+import flightLogic.saveTofiles as saveTofiles
+from Drivers.antennaDoor.AntennaDoor import AntennaDoor as antennaDoor
+from flightLogic.missionModes.antennaDeploy import antennaMode as antennaMode
+from flightLogic.missionModes.preBoomDeploy import preBoomMode
+from flightLogic.missionModes.boomDeploy import boomMode
+from flightLogic.missionModes.postBoomDeploy import postBoomMode
+from flightLogic.missionModes import safe
 import asyncio
+
 
 # from TXISR import interrupt
 # NOTE: The TXISR needs to run as a separate thread, and is not asyncio
@@ -23,138 +28,157 @@ import asyncio
 ##################################################################################################################
 
 
-
 def __main__():
-    executeFlightLogic()
+	asyncio.run(executeFlightLogic())
 
 
 async def executeFlightLogic():  # Open the file save object, start TXISR, and start Boot Mode data collection
-    # Variable setup
-    delay = 2100  # 35 minute delay = 2100 second delay
-    bootCount = 0
-    antennaDeployed = False
-    lastMode = "Boot" #Shawn, I don't know how you want this stored
-    boot = True
+	# Variable setup
+	delay = 1*60  # 35 minute delay
+	boot = True
+	save = saveTofiles.save()
+	# startTXISR(save)
+	ttncData = getDriverData.TTNCData(save)
+	attitudeData = getDriverData.AttitudeData(save)
+	safeMode = safe.safe(save)
 
-    save = saveTofiles.save()
-    #startTXISR(save)
-    taskAttitude = asyncio.create_task(getAttitude())
-    taskTTNC = asyncio.create_task(getTTNC())
-    # Initialize all mission mode objects
-    # NOTE: the comms-tx is the only exception to this rule as it is to be handled differently than other mission modes
-    # NOTE: Boot Mode is defined and executed in this document, instead of a separate mission mode
-    antennaDeploy = missionModes.antennaDeploy(save)
-    preBoomDeploy = missionModes.preBoomDeploy(save)
-    postBoomDeploy = missionModes.postBoomDeploy(save)
-    boomDeploy = missionModes.boomDeploy(save)
+	print('Starting data collection') #Setting up Background tasks for BOOT mode
+	tasks=[]
+	tasks.append(asyncio.create_task(ttncData.collectTTNCData(0))) #Boot Mode is classified as 0
+	tasks.append(asyncio.create_task(attitudeData.collectAttitudeData()))
+	tasks.append(asyncio.create_task(safeMode.thresholdCheck()))
+	tasks.append(asyncio.create_task(safeMode.heartBeat()))
 
-    bootCount, antennaDeployed, lastMode = readData()  # Read in data from files
-    bootCount += 1  # Increment boot count
-    recordData(bootCount, antennaDeployed, lastMode)
+	# Initialize all mission mode objects
+	# NOTE: the comms-tx is the only exception to this rule as it is to be handled differently than other mission modes
+	# NOTE: Boot Mode is defined and executed in this document, instead of a separate mission mode
+	antennaDeploy = antennaMode(save)
+	preBoomDeploy = preBoomMode(save)
+	postBoomDeploy = postBoomMode(save)
+	boomDeploy = boomMode(save)
 
-    # This is the implementation of the BOOT mode logic.
-    if not antennaDeployed:  # First, sleep for 35 minutes
-        await asyncio.sleep(delay)  # Sleep for 35 minutes
+	if(readData() == (None, None, None)):
+		print('Files are empty')
+		bootCount, antennaDeployed, lastMode = 0,False,0
+	else:
+		bootCount, antennaDeployed, lastMode = readData()  # Read in data from files
+	bootCount += 1  # Increment boot count
+	recordData(bootCount, antennaDeployed, lastMode)
 
-    # how do we check if the antenna doors are open?
-    # TODO, check of antenna doors are open
-    doorOpen = True
-    status = antennaDoor.readDoorStatus()
-    # this checks the bytes returned by the antennaDoor if any are 0 then doorOpen gets set to false
-    if not status & 0xf0:
-        doorOpen = False
-    else:
-        antennaDeployed = True
-    recordData()
+	if lastMode not in range(0,7): #Mission Mode invalid
+		lastMode = 0
+		antennaDeployed = False
 
-    if not doorOpen:
-        asyncio.create_task(antennaDeploy.run())
-        antennaDeployed = True
-        recordData() #Save into files
-    elif lastMode == "post boom deploy":
-        asyncio.create_task(postBoomDeploy.run())
-    else:
-        asyncio.create_task(preBoomDeploy.run())
+	# This is the implementation of the BOOT mode logic.
+	if not antennaDeployed:  # First, sleep for 35 minutes
+		print('Antenna is undeployed, waiting 35 minutes')
+		await asyncio.sleep(delay)  # Sleep for 35 minutes
 
-    try:  # Cancels attitude collection tasks
-        taskAttitude.cancel()
-        taskTTNC.cancel()
-    except asyncio.exceptions.CancelledError:
-        print("Exception thrown cancelling task - normal")
+	try:  # Cancels attitude collection tasks
+		for t in tasks:
+			t.cancel()
+		print('Successfully cancelled BOOT mode background tasks')
+	except asyncio.exceptions.CancelledError:
+		print("Exception thrown cancelling task - This is normal")
 
-    while True:  # This loop executes the rest of the flight logic
-        #pre boom deploy
-        if antennaDeployed == True and lastMode != "post boom deploy" and lastMode != "boom deploy":
-            lastMode = "pre boom deploy"
-            recordData()
-            asyncio.create_task(preBoomDeploy.run())  # Execute pre-boom deploy, then move to post-boom deploy
-            lastMode = "boom deploy"
-            recordData()
-        #boom deploy
-        elif antennaDeployed == True and lastMode == "boom deploy":
-            asyncio.create_task(boomDeploy.run())  # Execute boom deployment, start post-boom deploy
-            lastMode = "post boom deploy"
-            recordData()
-        else:  # Post-Boom Deploy
-            asyncio.create_task(postBoomDeploy.run())
+	# how do we check if the antenna doors are open?
+	# TODO, check of antenna doors are open
+	print('Moving on to check antenna door status')
+	status = antennaDoor().readDoorStatus()
+	# this checks the bytes returned by the antennaDoor if any are 0 then doorOpen gets set to false
+	if status != (1,1,1,1) and lastMode not in (2,3,4): #This will need to be changed to reflect the real antenna
+		antennaDeployed = False
+	else:
+        	antennaDeployed = True
+
+	recordData(bootCount, antennaDeployed, lastMode)
+
+	if not antennaDeployed:
+		print('Running Antenna Deployment Mode')
+		await asyncio.gather(antennaDeploy.run())
+		antennaDeployed = True
+		print(antennaDeployed)
+		recordData(bootCount, antennaDeployed, lastMode)  # Save into files
+	elif lastMode == 4:
+		print('Running Post Boom Deploy')
+		lastMode = 4
+		await asyncio.gather(postBoomDeploy.run())
+	else:
+		print('Running preBoom Deploy')
+		lastMode = 2
+		await asyncio.gather(preBoomDeploy.run())
+
+
+	while True: # This loop executes the rest of the flight logic
+	# pre boom deploy
+		if antennaDeployed == True and lastMode not in (3,4):
+			print('Running pre-Boom deploy')
+			lastMode = 2
+			recordData(bootCount, antennaDeployed, lastMode)
+			await asyncio.gather(preBoomDeploy.run())  # Execute pre-boom deploy, then move to post-boom deploy
+			lastMode = 3
+			recordData(bootCount, antennaDeployed, lastMode)
+		elif antennaDeployed == True and lastMode == 3:
+			print('Running Boom Deploy')
+			await asyncio.gather(boomDeploy.run())  # Execute boom deployment, start post-boom deploy
+			lastMode = 4
+			recordData(bootCount, antennaDeployed, lastMode)
+		else:  # Post-Boom Deploy
+			print('Running post-Boom Deploy')
+			await asyncio.gather(postBoomDeploy.run())
 
 
 def recordData(bootCount, antennaDeployed, lastMode):
+	# write to the boot file, "w" option in write overwrites the file
+	new = open(os.path.dirname(__file__) + "/bootRecords", "w+")
+	new.write(str(bootCount) + '\n')
+	if antennaDeployed:
+		new.write(str(1)+'\n')
+	else:
+		new.write(str(0)+'\n')
+	new.write(str(lastMode) + '\n')
+	new.close()
+
+	# write to the the back up file
+	new = open(os.path.dirname(__file__) + "/backupBootRecords", "w+")
+	new.write(str(bootCount) + '\n')
+	if antennaDeployed:
+		new.write(str(1)+'\n')
+	else:
+		new.write(str(0)+'\n')
+	new.write(str(lastMode) + '\n')
+	new.close()
 
 
-    # write to the boot file, "w" option in write overwrites the file
-    new = open("bootRecords", "w")
-    new.write(str(bootCount))
-    new.write(str(antennaDeployed))
-    new.write(str(lastMode))
-    new.close()
-
-    # write to the the back up file
-    new = open("backupBootRecords", "w")
-    new.write(str(bootCount))
-    new.write(str(antennaDeployed))
-    new.write(str(lastMode))
-    new.close()
-
-
-def readData():  # This function reads in data from the files, it was previously in the main function but is better as its own function
-    # bootRecords file format
-    # Line 1 = boot count
-    # Line 2 = first boot
-    # Line 3 = antenna deployed?
-    # Line 4 = last mission mode
-    # the try except is a way to back up our files, if one is corrupted the other used
-    try:
-        bootFile = open("bootRecords", "r")
-        bootCount = int(bootFile.readline())
-        antennaDeployed = bool(bootFile.readline())
-        lastMode = str(bootFile.readline())
-        bootFile.close()
-    except:
-        bootFile = open("backupBootRecords", "r")
-        bootCount = int(bootFile.readline())
-        antennaDeployed = bool(bootFile.readline())
-        lastMode = str(bootFile.readline())
-        bootFile.close()
-        # In this except statement, the files are corrupted, so we rewrite both of them
-        recordData()
-    return bootCount, antennaDeployed, lastMode
+def readData():
+	# This function reads in data from the files, it was previously in the main function but is better as its own function
+	# bootRecords file format
+	# Line 1 = boot count
+	# Line 2 = antenna deployed?
+	# Line 2 = last mission mode
+	bootCount,antennaDeployed,lastMode = None, None, None
+	try:
+		bootFile = open(os.path.dirname(__file__) + "/bootRecords", "r")
+		bootCount = int(bootFile.readline().rstrip())
+		antennaDeployed = bool(int(bootFile.readline().rstrip()))
+		lastMode = int(bootFile.readline().rstrip())
+		bootFile.close()
+	except:
+		try:
+			print('File exception')
+			bootFile = open(os.path.dirname(__file__) + "/backupBootRecords", "r")
+			bootCount = int(bootFile.readline().rstrip())
+			antennaDeployed = bool(int(bootFile.readline().rstrip()))
+			lastMode = int(bootFile.readline().rstrip())
+			bootFile.close()
+			# In this except statement, the files are corrupted, so we rewrite both of them
+		except:
+			print('Double File exception - are both files non-existant?')	
+	recordData(bootCount, antennaDeployed, lastMode)
+	return bootCount, antennaDeployed, lastMode
 
 
-#def startTXISR(saveobject):  # Setup for TXISR
-    # This sets up the interupt on the uart pin that triggers when we get commincation over uart
-    #thread.start(interrupt.watchReceptions(saveobject)) <-- TODO fix that import
+# def startTXISR(saveobject):  # Setup for TXISR
+# This sets up the interupt on the uart pin that triggers when we get commincation over uart
+# thread.start(interrupt.watchReceptions(saveobject)) <-- TODO fix that import
 
-# These functions collect data in the background during BOOT mode, if applicable
-async def getTTNC():
-    ttnc = TTNCData()
-    while True:
-        ttnc.getData(0)  # BOOT is mission mode 0
-        await asyncio.sleep(120) #0.0083HZ
-
-
-async def getAttitude():
-    attitude = AttitudeData()
-    while True:
-        attitude.getData()
-        await asyncio.sleep(1) #1HZ
