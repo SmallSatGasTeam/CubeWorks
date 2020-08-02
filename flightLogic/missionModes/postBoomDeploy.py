c086a97360986d06f44770ca70a83cfc8ec7aa61
loganfreeman1 <67300766+loganfreeman1@users.noreply.github.com>
2020-08-02 14:40:23 -0600

    Update postBoomDeploy.py

--- flightLogic/missionModes/postBoomDeploy.py
+++ flightLogic/missionModes/postBoomDeploy.py
@@ -1,6 +1,8 @@
+import sys
+sys.path.append('../../')
 import asyncio
-from .safe import safe
-from ..getDriverData import *
+from flightLogic.missionModes import safe
+from flightLogic.getDriverData import *
 import Drivers.eps.EPS as EPS
 
 class postBoomMode:
@@ -9,27 +11,29 @@ class postBoomMode:
 		self.__getDataTTNC = TTNCData(saveobject)
 		self.__getDataAttitude =  AttitudeData(saveobject)
 		self.__tasks = [] # List will be populated with all background tasks
-		self.__safeMode = safe(saveobject)
+		self.__safeMode = safe.safe(saveobject)
 
 	async def run(self):
 		#Set up background processes
 		ttncData = self.__getDataTTNC
 		attitudeData = self.__getDataAttitude
 		self.__tasks.append(asyncio.create_task(ttncData.collectTTNCData(4))) #Post-boom is mode 4
-		self.__tasks.append(asyncio.create_task(attitudeData.collectAttitudeData))
+		self.__tasks.append(asyncio.create_task(attitudeData.collectAttitudeData()))
 		self.__tasks.append(asyncio.create_task(self.__safeMode.thresholdCheck()))
-		self.__tasks.append(asyncio.create_task(self.rebootLoop()))
-
-
-
-	async def rebootLoop(self):
-		while True:
-			upTime = 0
+		self.__tasks.append(asyncio.create_task(self.__safeMode.heartBeat()))
+		upTime = 86100
+		while True: #Runs reboot loop
 			if upTime>86400: #Live for more than 24 hours
 				self.__safeMode.run(1) #restart, powering off Pi for 1 minute
+				print('Rebooting raspberry pi')
+				upTime = 0 # Won't be necessary for flight article
 			else:
-				upTime += 60
+				print('Uptime: '+str(upTime))
 				await asyncio.sleep(60)
+				upTime += 60
+
+
+
 
 	def cancellAllTasks(self, taskList): #Isn't used in this class, but here anyways
 		try:
