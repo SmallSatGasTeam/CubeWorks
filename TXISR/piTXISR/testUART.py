cba8fbc4717462c75f22c60d5e3f2d44823bc80e
ShawnBraden <sbradenjones16@gmail.com>
2020-08-03 22:47:41 -0600

    Testing uart

--- TXISR/piTXISR/testUART.py
+++ TXISR/piTXISR/testUART.py
@@ -1,9 +1,13 @@
 import serial
+import os 
+import sys
+TRANSMIT_EXE = "TXServiceCode/TXService.run"
+
 print("beging")  
 ser = serial.Serial ("/dev/ttyAMA0")  
 print("Opening port")    
 ser.baudrate = 9600                   
-ser.write(str.encode("hello"))  
+os.system(TRANSMIT_EXE + " 1") 
 print("\nsending")                     
 data = ser.read(5)
 print(data)                      
