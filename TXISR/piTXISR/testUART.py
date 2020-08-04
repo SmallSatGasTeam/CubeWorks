693f90cc3e74852011efc1a750903b4f433812d3
ShawnBraden <sbradenjones16@gmail.com>
2020-08-03 22:59:06 -0600

    Testing uart

--- TXISR/piTXISR/testUART.py
+++ TXISR/piTXISR/testUART.py
@@ -9,6 +9,6 @@ print("Opening port")
 ser.baudrate = 9600                   
 os.system(TRANSMIT_EXE + " 1") 
 print("\nsending")                     
-data = ser.read(5)
+data = ser.read()
 print(data)                      
 ser.close() 
