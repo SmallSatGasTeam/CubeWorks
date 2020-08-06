b3426bdffa1f6032aff78a366c8baeabd7a15de7
ShawnBraden <sbradenjones16@gmail.com>
2020-08-05 19:22:26 -0600

    Adding c to uart

--- TXISR/piTXISR/testUART.py
+++ TXISR/piTXISR/testUART.py
@@ -10,7 +10,7 @@ print("Opening port")
 ser.baudrate = 115200                   
 os.system(TRANSMIT_EXE + " 1") 
 print("\nPython Reading")  
-while not os.system(read_EXE) : 
+while not (checking = os.system(read_EXE)) : 
     print(checking)                    
 data = ser.read(100)
 print(data)                      
