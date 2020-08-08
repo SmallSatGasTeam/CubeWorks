c02bf4bb340382efe7437ff9ba3434cceeac899e
ShawnBraden <sbradenjones16@gmail.com>
2020-08-08 11:40:24 -0600

    fixing the tx interrupt

--- TXISR/callInterrupt.py
+++ TXISR/callInterrupt.py
@@ -4,7 +4,7 @@ from interrupt import INTERRUPT
 async def testInterrupt():
 	interruptObject = INTERRUPT()
 	tasks = []
-	tasks.append(asyncio.create_task(interruptObject.watchTxWindows()))
+	#tasks.append(asyncio.create_task(interruptObject.watchTxWindows()))
 	tasks.append(asyncio.create_task(interruptObject.watchReceptions()))
 
 asyncio.run(testInterrupt())
