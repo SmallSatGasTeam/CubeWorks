efc49b72f7ff83bd0e1440f14971d0b1f33a5b25
ShawnBraden <sbradenjones16@gmail.com>
2020-08-08 13:55:40 -0600

    fixing the rx asynic func call

--- TXISR/callInterrupt.py
+++ TXISR/callInterrupt.py
@@ -6,11 +6,11 @@ async def testInterrupt():
 	tasks = []
 	tasks.append(asyncio.create_task(interruptObject.watchReceptions()))
 	tasks.append(asyncio.create_task(interruptObject.watchTxWindows()))
-	while true :
-		if interruptObject.getRXSTatous() :
-			tasks[0] = (asyncio.create_task(interruptObject.watchReceptions()))
-		if interruptObject.getTXstatus() :
-			#tasks[1] = (asyncio.create_task(interruptObject.watchTxWindows()))
+	# while true :
+		# if interruptObject.getRXSTatous() :
+		# 	tasks[0] = (asyncio.create_task(interruptObject.watchReceptions()))
+		# if interruptObject.getTXstatus() :
+		# 	#tasks[1] = (asyncio.create_task(interruptObject.watchTxWindows()))
 
 
 asyncio.run(testInterrupt())
