aeed1d8a40c5ee7cdb1c9f4897e8859fbc2cfdec
Logan Freeman <lfreeman@gas.edu.usu>
2020-08-14 11:49:39 -0600

    Fixing tests

--- tests/testAccel.py
+++ tests/testAccel.py
@@ -3,21 +3,8 @@ import unittest
 import sys
 sys.path.append("../")
 
-from Drivers.Accelerometer import Accelerometer
 
-class ADCTest(unittest.TestCase):
-    accel = Accelerometer()
-    """
-    A test case containing sets of assertions in the form of class methods.
-    """
-    def testAccel(self):
-        """Asserts that the magnitutde of the vector is between 9.5 and 10.1.  Assertion should pass"""
-        self.assertTrue(np.linalg.norm(mag.read()) >= 9.5 && np.linalg.norm(mag.read()) <= 10.1)
-    
-
-    
-#if __name__ == '__main__':
-accel = Accelerometer()
-    #unittest.main()
-print(accel.read())
-print(np.linalg.norm(accel.read()))
+if __name__ == '__main__':
+	accel = Accelerometer()
+	print(accel.read())
+	print(np.linalg.norm(accel.read())
