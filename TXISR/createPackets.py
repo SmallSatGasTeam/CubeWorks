04cc46e93b072075aa562c1b17d905d84b4de961
loganfreeman1 <67300766+loganfreeman1@users.noreply.github.com>
2020-08-17 17:42:02 -0600

    Update createPackets.py

--- TXISR/createPackets.py
+++ TXISR/createPackets.py
@@ -1,9 +1,15 @@
-import struct
 
 typeOfPacket = input('Type 0 for Window Packet, 1 for Command Packet: ')
 if(typeOfPacket == '0'):
 	#Window packet
-	pass
+	packet = '0'
+	packet += int4tobin(int(input('Input the number of seconds until window start: ')))
+	packet += int2tobin(int(input('Input the duration of the window in seconds: ')))
+	packet += int1tobin(int(input('Number from 0-4 corresponding to requested data type. See flight logic document: ')))
+	packet += int2tobin(int(input('If picture is requested, number of the picture that is requested: ')))
+	packet += '0000000'
+	print(hex(int(packet, 2))[2:].zfill(20))
+
 else:
 	#Command Packet
 	commandsList = []
@@ -21,30 +27,15 @@ else:
 			content += '1'
 	content += '0'
 	print(hex(int(content, 2))[2:].zfill(2))
-	
 
-
-
-
-
-
-
-def float4tohex(num):
-	#takes a 4 byte float, returns a hex representation of it
-	return str(hex(struct.unpack('<I', struct.pack('<f', num))[0]))[2:]
-
-def int4tohex(num):
-	#takes a 4 byte int, returns a hex representation of it
-	return str(format(num, '08x'))[-8:]
+def int4tobin(num):
+	#takes a 4 byte int, returns a binary representation of it
+	return str(format(num, '08b'))[-8:]
 
 def int1tohex(num):
-	#takes a 1 byte integer, returns a hex representation of it
-	return str(format(num, '02x'))[-2:]
+	#takes a 1 byte integer, returns a binary representation of it
+	return str(format(num, '02b'))[-2:]
 
 def int2tohex(num):
-	#takes a 2 byte integer, returns a hex representation of it
-	return str(format(num, '04x'))[-4:]
-
-def int8tohex(num):
-	#takes an 8 byte integer, returns a hex representation of it
-	return str(format(num, '016x'))[-16:]
+	#takes a 2 byte integer, returns a binary representation of it
+	return str(format(num, '04b'))[-4:]
