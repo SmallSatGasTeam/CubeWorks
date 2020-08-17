ff7d4a0dd3a1de3c90a91cfe8e8cc424ae327f5d
Logan Freeman <lfreeman@gas.edu.usu>
2020-08-16 18:08:06 -0600

    Clear bugged packets

--- TXISR/pythonInterrupt.py
+++ TXISR/pythonInterrupt.py
@@ -12,14 +12,21 @@ To defray the possibility of half a packet being in the buffer, any half-packets
 async def interrupt():
 	serialport = serial.Serial('/dev/serial0', 115200) #Open serial port. Currently /dev/serial0, might change to the PL011 port for flight article
 	leftovers = '' #Stores any half-packets for evaluation the next loop
+	leftoversEmpty = True
 	gaspacsHex = str(b'GASPACS'.hex())
 	while True:
 		if serialport.in_waiting: #If there is content in the serial buffer, read it and act on it
 			print('Data in waiting')
 			data = str(serialport.read(serialport.inWaiting()).hex()) #This produces a list of nibbles (half bytes)
 			data = leftovers+data #Append any leftover data for evaluation
+			if leftovers is not '':
+				leftoverEmpty = False
 			commands, ax25Packets = [], []
 			commands, ax25Packets, leftovers = parseData(data, gaspacsHex)
+			if leftovers is not '' and leftoversEmpty is False:
+				#Something is sticking around in leftovers, let's clear it
+				#Operates on the assumption that 2 consecutive partial packets is practically impossible
+				leftovers = ''
 			for command in commands:
 				print(command)
 				packetProcessing.processPacket(command) #Process Command Packets
