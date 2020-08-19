313406fd94c8f5afe2403fbec3ff9c7a3c7b4dd0
Logan Freeman <lfreeman@gas.usu.edu>
2020-08-19 17:09:40 +0100

    working on testTransmissions file

--- tests/testTransmissions.py
+++ tests/testTransmissions.py
@@ -3,6 +3,10 @@ import sys
 sys.path.append('../')
 from TXISR import pythonInterrupt
 from TXISR import packetProcessing
-
+from TXISR import prepareFiles
 #This file duplicates the functionality of POST-BOOM deploy as it relates to communications
 
+if __name__ = '__main__':
+	asyncio.create_task(pythonInterrupt.interrupt())
+	asyncio.create_task(readNextTransferWindow())
+	asyncio.create_task(rebootLoop())
