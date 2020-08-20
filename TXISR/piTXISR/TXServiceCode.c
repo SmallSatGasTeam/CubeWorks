4890f330bfe6916acc1ab94d65012bd84a42a8e9
ShawnBraden <sbradenjones16@gmail.com>
2020-08-19 20:30:47 -0600

    adding hex

--- TXISR/piTXISR/TXServiceCode.c
+++ TXISR/piTXISR/TXServiceCode.c
@@ -209,7 +209,7 @@ void main(int argc,char* argv[])
         //convert the data to hex
         int temp = 0;
         PRINT_DEBUG(charCount / 2)
-        for(int count = 0; count - 1 <= charCount / 2;)
+        for(int count = 0; count - 10 <= charCount / 2;)
         {
             PRINT_DEBUG(count)
             sscanf(&line[temp], "%2hhx", &sendingData[count]);
