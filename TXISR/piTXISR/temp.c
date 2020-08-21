55a4e1b9728ed46f39416d7898b718f157fa0d95
ShawnBraden <sbradenjones16@gmail.com>
2020-08-20 20:54:44 -0600

    I hope this works!

--- TXISR/piTXISR/temp.c
+++ TXISR/piTXISR/temp.c
@@ -196,7 +196,6 @@ void main(int argc,char* argv[])
             //save all the data in that line
             //this if lets us not send the line number if this is a photo file
             if(end && ch != TIME_DEVISOR)line[charCount++] = ch;
-            else if(dataType != PHOTO_TYPE && ch == TIME_DEVISOR) line[charCount++] = ',';
             //PRINT_DEBUG_c(ch)
             //DEBUG_P(Im in the sub loop)
         }while(ch != 10 && !feof(txFile));
