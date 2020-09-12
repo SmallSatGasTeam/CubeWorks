441c2a38adf6fccf3794ce761c64824571f08242
Jack Danos <jackadanos@gmail.com>
2020-09-12 15:11:49 -0600

    working on flight logic

--- tests/testMainFlightLogic.py
+++ tests/testMainFlightLogic.py
@@ -2,4 +2,8 @@ import sys
 import asyncio
 sys.path.append('../')
 from flightLogic import mainFlightLogic
+log = open("../logs/mainLog.log", "w")
+sys.stdout = log
+sys.stderr = log
+print("Flight Logic Start")
 asyncio.run(mainFlightLogic.executeFlightLogic())
