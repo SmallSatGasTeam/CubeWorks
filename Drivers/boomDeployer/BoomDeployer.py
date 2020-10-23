ccc5bf77309421219fd3bb2e84e40e5b40b8b6bd
Jack Danos <jackadanos@gmail.com>
2020-10-23 01:46:00 -0600

    updated boom deploy driver to use PWM. Still needs to be physically tested

--- Drivers/boomDeployer/BoomDeployer.py
+++ Drivers/boomDeployer/BoomDeployer.py
@@ -14,6 +14,8 @@ class BoomDeployer(Driver):
         self.burnTimeWC2 = 5
         self.waitTime = 10
         self.numTimes = 3
+        self.dutyCycle1 = 75
+        self.dutyCycle2 = 90
 
         # Set up the GPIO pins for use
         GPIO.setmode(GPIO.BCM)
@@ -25,9 +27,12 @@ class BoomDeployer(Driver):
         self.wireCutter1_high2 = 16
 	# BOARD 7 is GPIO 4
         self.wireCutter1_low1 = 4
+
+	# Set up Wirecutter 1 pins
         GPIO.setup(self.wireCutter1_high1, GPIO.OUT, initial=GPIO.LOW)
         GPIO.setup(self.wireCutter1_high2,GPIO.OUT, initial=GPIO.LOW)
         GPIO.setup(self.wireCutter1_low1,GPIO.OUT, initial=GPIO.HIGH)
+        self.PWM1 = GPIO.PWM(self.wireCutter1_high1, 500)
 
         #Second Wirecutter
 	# BOARD 37 is GPIO 26
@@ -36,28 +41,34 @@ class BoomDeployer(Driver):
         self.wireCutter2_high2 = 19
 	# BOARD 29 is GPIO 5
         self.wireCutter2_low1 = 5
+
+	# Set up Wirecutter 2 pins
         GPIO.setup(self.wireCutter2_high1, GPIO.OUT, initial=GPIO.LOW)
         GPIO.setup(self.wireCutter2_high2,GPIO.OUT, initial=GPIO.LOW)
         GPIO.setup(self.wireCutter2_low1,GPIO.OUT, initial=GPIO.HIGH)
+        self.PWM2 = GPIO.PWM(self.wireCutter2_high1, 500)
 
 
     async def deploy(self):
         """
         Loop a specified number of times, setting the correct GPIO pins to HIGH/LOW  to start/stop
-	the burn. Wait and then repeat with the other wirecutter mechanism
+	the burn. Wait and then repeat with the other wirecutter mechanism.
+	Note: PWM is used on only one channel.
         """
         for num in range(0, self.numTimes):
-            
+
             #Turn on Wire Cutter 1
             GPIO.output(self.wireCutter1_high1, GPIO.HIGH)
             GPIO.output(self.wireCutter1_high2, GPIO.HIGH)
             GPIO.output(self.wireCutter1_low1, GPIO.LOW)
+            self.PWM1.start(self.dutyCycle1)
             #Burn for set number of seconds
             await asyncio.sleep(self.burnTimeWC1)
             #Turn off Wire Cutter 1
             GPIO.output(self.wireCutter1_high1, GPIO.LOW)
             GPIO.output(self.wireCutter1_high2, GPIO.LOW)
             GPIO.output(self.wireCutter1_low1, GPIO.HIGH)
+            self.PWM1.stop()
             #Wait
             await asyncio.sleep(self.waitTime)
 
@@ -65,12 +76,14 @@ class BoomDeployer(Driver):
             GPIO.output(self.wireCutter2_high1, GPIO.HIGH)
             GPIO.output(self.wireCutter2_high2, GPIO.HIGH)
             GPIO.output(self.wireCutter2_low1, GPIO.LOW)
+            self.PWM2.start(self.dutyCycle2)
             #Burn for set number of seconds
             await asyncio.sleep(self.burnTimeWC2)
             #Turn off Wire Cutter 2
             GPIO.output(self.wireCutter2_high1, GPIO.LOW)
             GPIO.output(self.wireCutter2_high2, GPIO.LOW)
             GPIO.output(self.wireCutter2_low1, GPIO.HIGH)
+            self.PWM2.stop()
             #Wait
             await asyncio.sleep(self.waitTime)
 
