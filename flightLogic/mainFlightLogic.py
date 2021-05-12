bd6184c9cc924c93ffa2b63620529237c30acd12
evanmand <evanmand02@gmail.com>
2021-05-12 10:27:52 -0600

    testing

--- flightLogic/mainFlightLogic.py
+++ flightLogic/mainFlightLogic.py
@@ -202,6 +202,7 @@ def readData():
 	recordData(bootCount, antennaDeployed, lastMode)
 	return bootCount, antennaDeployed, lastMode
 
+__main__()
 
 # def startTXISR(saveobject):  # Setup for TXISR
 # This sets up the interupt on the uart pin that triggers when we get commincation over uart
