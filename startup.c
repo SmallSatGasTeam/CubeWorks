7d66de86994edc561d54245707eeda2a433962ff
Joshua Hessing <jjhjjh7@gmail.com>
2021-05-13 09:16:42 -0600

    Added startup.c and startup.exe to the codeBase

---
+++ startup.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    FILE *lastBase;
+    char codeBase;
+    int i;
+
+    printf("About to open lastBase.\n");
+
+    lastBase = fopen("lastBase.txt", "r");
+    if(lastBase == NULL){
+        codeBase = '0';
+        printf("Failed to find the file.\n");
+    }
+    else {
+        codeBase = getc(lastBase);
+        fclose(lastBase);
+    }
+
+    if(codeBase >= '4') codeBase = '0';
+    else codeBase++;
+
+    lastBase = fopen("lastBase.txt", "w");
+    fprintf(lastBase, "%c", codeBase);
+
+    //I needed a comment in here so I can push
+    switch(codeBase) {
+        case '0': printf("Running code base 0.\n"); system("cd CubeWorks0/tests\nsudo python3 testMainFlightLogicNOTSub.py");
+            break;
+        case '1': printf("Running code base 1.\n"); system("cd CubeWorks1/tests\nsudo python3 testMainFlightLogicNOTSub.py");
+            break;
+        case '2': printf("Running code base 2.\n"); system("cd CubeWorks2/tests\nsudo python3 testMainFlightLogicNOTSub.py");
+            break;
+        case '3': printf("Running code base 3.\n"); system("cd CubeWorks3/tests\nsudo python3 testMainFlightLogicNOTSub.py");
+            break;
+        case '4': printf("Running code base 4.\n"); system("cd CubeWorks4/tests\nsudo python3 testMainFlightLogicNOTSub.py");
+            break;
+        default: printf("Running code base 0.\n"); system("cd CubeWorks0/tests\nsudo python3 testMainFlightLogicNOTSub.py");
+            break;
+    }
+
+    fclose(lastBase);
+
+    return 0;
+}
