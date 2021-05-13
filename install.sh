47cad26cf636384b504c97081df0061d7cb6b383
ShawnBraden <sbradenjones16@gmail.com>
2021-05-13 10:25:07 -0600

    adding install process

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #this will handle the install of all gas software
-$ sudo apt full-upgrade
-$ sudo apt install python3
-$ sudo apt install python3-pip
-$ sudo apt install python3-numpy
-$ cd ; cd CubeWorks ; pip3 install -r requirements.txt ; cd ;
+sudo apt full-upgrade
+sudo apt install python3
+sudo apt install python3-pip
+sudo apt install python3-numpy
+cd ; cd ~/CubeWorks ; pip3 install -r requirements.txt ; cd ;
