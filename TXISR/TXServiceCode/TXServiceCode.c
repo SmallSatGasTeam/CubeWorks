582f809f307ecbaeafd8e727a5ba462cd332198f
Joshua Hessing <jjhjjh7@gmail.com>
2021-05-13 09:13:49 -0600

    Changed the file paths in TXISR

--- TXISR/TXServiceCode/TXServiceCode.c
+++ TXISR/TXServiceCode/TXServiceCode.c
@@ -16,7 +16,7 @@
 #define ENABLE "./configPinsTXISR"
 #define DISABLE "./configPinsTXISRDone"
 
-#define FLAG_FILE "../TXISR/data/flagsFile.txt" //change this later for the real program
+#define FLAG_FILE "../../TXISRData/flagsFile.txt" //change this later for the real program
 #define FORMAT_FILE "../TXISR/data/txFile.txt" //this is the file that dallan will creat
 #define UART_PORT "/dev/serial0" //this is serial port name, make sure this is correct for the final code
 
