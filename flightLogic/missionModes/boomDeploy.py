809e10e341ef251043301c35970259a95a6aca4a
evanmand <evanmand02@gmail.com>
2021-05-19 11:39:57 -0600

    I missed one more closing line for last commit V

--- flightLogic/missionModes/boomDeploy.py
+++ flightLogic/missionModes/boomDeploy.py
@@ -86,6 +86,7 @@ class boomMode:
 						soonestWindowTime = float(data[0]) - time.time()
 						sendData = data
 						print(sendData)
+			transferWindowFilename.close()
 			if not(sendData == 0):
 				#print("Found next transfer window: ")
 				#print(sendData)
