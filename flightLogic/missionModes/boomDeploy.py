90e5438c9f4bb629c901ebdfc0961422974d7d0d
evanmand <evanmand02@gmail.com>
2021-05-19 10:45:23 -0600

    Duplicated readNextTransferWindow into antennaDeploy, preBoomDeploy, and boomDeploy

--- flightLogic/missionModes/boomDeploy.py
+++ flightLogic/missionModes/boomDeploy.py
@@ -9,7 +9,7 @@ from Drivers.camera import Camera
 from TXISR import pythonInterrupt
 from TXISR import packetProcessing
 
-
+TRANSFER_WINDOW_BUFFER_TIME = 10 #30 seconds
 
 class boomMode:
 	def __init__(self, saveObject, safeModeObject):
@@ -56,8 +56,47 @@ class boomMode:
 			print("Caught thrown exception in cancelling background task")
 
 	async def skipToPostBoom(self):
+		print("Inside skipToPostBoom, skipping value is:", packetProcessing.skippingToPostBoom)
 		if packetProcessing.skippingToPostBoom:
 			self.cancelAllTasks(self.__tasks)
 			return True
 		else:
 			await asyncio.sleep(1)
+
+	async def readNextTransferWindow(self, transferWindowFilename):
+		while True:
+			print("Inside transfer window.")
+			#read the given transfer window file and extract the data for the soonest transfer window
+			fileChecker.checkFile(transferWindowFilename)
+			transferWindowFile = open(transferWindowFilename)
+			sendData = 0
+			soonestWindowTime = 0
+			for line in transferWindowFile:
+				print("reading line: ")
+				print(line)
+				data = line.split(",")
+				print(data)
+				#data[0] = time of next window, data[1] = duration of window, data[2] = datatype, data[3] = picture number
+				print(float(data[0]), float(data[0]) - time.time(), TRANSFER_WINDOW_BUFFER_TIME)
+				if(float(data[0]) - time.time() > TRANSFER_WINDOW_BUFFER_TIME):  #if the transfer window is at BUFFER_TIME milliseconds in the future
+					if(soonestWindowTime == 0 or float(data[0]) - time.time() < soonestWindowTime):
+						soonestWindowTime = float(data[0]) - time.time()
+						sendData = data
+						print(sendData)
+			if not(sendData == 0):
+				#print("Found next transfer window: ")
+				#print(sendData)
+				self.__timeToNextWindow = float(sendData[0]) - time.time()
+				self.__duration = int(sendData[1])
+				self.__datatype = int(sendData[2])
+				self.__pictureNumber = int(sendData[3])
+				self.__nextWindowTime = float(sendData[0])
+				self.__startFromBeginning = bool(sendData[4])
+				self.__index = int(sendData[5])
+				# print(self.__startFromBeginning)
+				# print(self.__timeToNextWindow)
+				# print(self.__duration)
+				# print(self.__datatype)
+				# print(self.__pictureNumber)
+				# print(self.__index)
+			await asyncio.sleep(3) #Checks transmission windows every 10 seconds
--- flightLogic/missionModes/preBoomDeploy.py
+++ flightLogic/missionModes/preBoomDeploy.py
@@ -1,4 +1,5 @@
 import sys
+import time
 sys.path.append('../../')
 import asyncio
 from flightLogic.missionModes import safe
@@ -16,6 +17,8 @@ sunSensorMin = 0.0
 sunSensorMax = 3.3
 getBusVoltageMin = 3.5
 getBusVoltageMax = 5.1
+
+TRANSFER_WINDOW_BUFFER_TIME = 10 #30 seconds
 
 DummySunSensor = True
 DEBUG = False
@@ -156,11 +159,50 @@ class preBoomMode:
 			print("Caught thrown exception in cancelling background task")
 	
 	async def skipToPostBoom(self):
+		print("Inside skipToPostBoom, skipping value is:", packetProcessing.skippingToPostBoom)
 		if packetProcessing.skippingToPostBoom:
 			self.cancelAllTasks(self.__tasks)
 			return True
 		else:
 			await asyncio.sleep(1)
+
+	async def readNextTransferWindow(self, transferWindowFilename):
+		while True:
+			print("Inside transfer window.")
+			#read the given transfer window file and extract the data for the soonest transfer window
+			fileChecker.checkFile(transferWindowFilename)
+			transferWindowFile = open(transferWindowFilename)
+			sendData = 0
+			soonestWindowTime = 0
+			for line in transferWindowFile:
+				print("reading line: ")
+				print(line)
+				data = line.split(",")
+				print(data)
+				#data[0] = time of next window, data[1] = duration of window, data[2] = datatype, data[3] = picture number
+				print(float(data[0]), float(data[0]) - time.time(), TRANSFER_WINDOW_BUFFER_TIME)
+				if(float(data[0]) - time.time() > TRANSFER_WINDOW_BUFFER_TIME):  #if the transfer window is at BUFFER_TIME milliseconds in the future
+					if(soonestWindowTime == 0 or float(data[0]) - time.time() < soonestWindowTime):
+						soonestWindowTime = float(data[0]) - time.time()
+						sendData = data
+						print(sendData)
+			if not(sendData == 0):
+				#print("Found next transfer window: ")
+				#print(sendData)
+				self.__timeToNextWindow = float(sendData[0]) - time.time()
+				self.__duration = int(sendData[1])
+				self.__datatype = int(sendData[2])
+				self.__pictureNumber = int(sendData[3])
+				self.__nextWindowTime = float(sendData[0])
+				self.__startFromBeginning = bool(sendData[4])
+				self.__index = int(sendData[5])
+				# print(self.__startFromBeginning)
+				# print(self.__timeToNextWindow)
+				# print(self.__duration)
+				# print(self.__datatype)
+				# print(self.__pictureNumber)
+				# print(self.__index)
+			await asyncio.sleep(3) #Checks transmission windows every 10 seconds
 
 class unexpectedValue(Exception):
 	print("Received unexpected value.")
