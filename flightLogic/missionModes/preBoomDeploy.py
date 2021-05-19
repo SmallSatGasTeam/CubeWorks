3e31547e9e722cbc65504a8b47041901c8fafbb8
aallgrunn <aallgrunn@hotmail.com>
2021-05-19 12:01:01 -0600

    Fixed file close in preBoom

--- flightLogic/missionModes/preBoomDeploy.py
+++ flightLogic/missionModes/preBoomDeploy.py
@@ -187,8 +187,8 @@ class preBoomMode:
 					if(soonestWindowTime == 0 or float(data[0]) - time.time() < soonestWindowTime):
 						soonestWindowTime = float(data[0]) - time.time()
 						sendData = data
-						print(sendData)()
-			transferWindowFilename.close
+						print(sendData)
+			transferWindowFilename.close()
 			if not(sendData == 0):
 				#print("Found next transfer window: ")
 				#print(sendData)
