e72a7e24051a594a80f397556ca22d1df37338bf
aallgrunn <aallgrunn@hotmail.com>
2021-05-19 12:43:48 -0600

    made 0 NULL

--- flightLogic/missionModes/postBoomDeploy.py
+++ flightLogic/missionModes/postBoomDeploy.py
@@ -114,16 +114,15 @@ class postBoomMode:
 				#data[0] = time of next window, data[1] = duration of window, data[2] = datatype, data[3] = picture number
 				print(float(data[0]), float(data[0]) - time.time(), TRANSFER_WINDOW_BUFFER_TIME)
 				if(float(data[0]) - time.time() > TRANSFER_WINDOW_BUFFER_TIME):  #if the transfer window is at BUFFER_TIME milliseconds in the future
-					print("WE ARE GETTING TO THE PLACE")
-					print("soonestWindowTime: " + str(soonestWindowTime))
 					if(soonestWindowTime == 0 or float(data[0]) - time.time() < soonestWindowTime):
 						soonestWindowTime = float(data[0]) - time.time()
 						sendData = data
 						print("Data: " + str(data))
 						print(str(sendData))
+
 			transferWindowFilename.close()
 
-			if not(sendData == 0):
+			if not(sendData == NULL):
 				#print("Found next transfer window: ")
 				#print(sendData)
 				self.__timeToNextWindow = float(sendData[0]) - time.time()
