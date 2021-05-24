a1baea7a598d22de419bbaf22718801d24a39492
Joshua Hessing <jjhjjh7@gmail.com>
2021-05-24 13:14:17 -0600

    Finding TTNC data and fixing transmitting.py

--- flightLogic/missionModes/transmitting.py
+++ flightLogic/missionModes/transmitting.py
@@ -43,6 +43,8 @@ class Transmitting:
                 line = self.__transferWindowFile.readline()
                 data = line.split(",")
                 searchTime = data[0]
+
+            print("Outside of the whlie statement.")
             #data[0] = time of next window, data[1] = duration of window, data[2] = datatype, data[3] = picture number, data[4] = line index
             try:
                 if (data != ['']) or (data != []):
