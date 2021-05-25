98f66dad8ebd569f38431bdc34cc89f9ce87cd45
ShawnBraden <sbradenjones16@gmail.com>
2021-05-25 11:14:25 -0600

    add queue to the tx logic

--- TXISR/transmitionQueue.py
+++ TXISR/transmitionQueue.py
@@ -5,10 +5,10 @@ class Queue ():
     def __init__(self, filepath):
         print("Making the queue")
         self.__filepath = filepath
-        #self.__fileChecher = FileReset()
+        self.__fileChecher = FileReset()
 
     def enqueue(self, data):
-        #self.__fileChecher.checkFile(self.__filepath)
+        self.__fileChecher.checkFile(self.__filepath)
         file = open(self.__filepath, "a+") 
         file.write(str(data) + "\n")
         file.close()
@@ -16,27 +16,46 @@ class Queue ():
     def dequeue(self):
         return self.__short()
 
+
     def incramentTime(self):
-        pass
+        contents = []
+        self.__fileChecher.checkFile(self.__filepath)
+        file = open(self.__filepath, "r") 
+        contents = file.read().splitlines()
+        file.close()
+        for i in range (len(contents)):
+            contents[i] = str(int (contents[i]) - 1)
+        file = open(self.__filepath, "w")
+        for i in contents:
+            file.write(i + "\n")
+        file.close()
+
 
     def __short(self):
         contents = []
-        #self.__fileChecher.checkFile(self.__filepath)
+        self.__fileChecher.checkFile(self.__filepath)
         file = open(self.__filepath, "r") 
         contents = file.read().splitlines()
         file.close()
         print(contents)
+        if(len(contents) == 0):
+            return None
         if(contents[0] != -1 ):
             min = int (contents[0])
             for i in contents:
-                if(int(i) < min and int(i) != -1): 
+                if(int(i) < min and int(i) >= 0): 
                     min = int(i)
             contents.remove(str (min))
             file = open(self.__filepath, "w") 
             for j in contents:
-                file.write(j + "\n")
+                if(int(j) >= 0):
+                    file.write(j + "\n")
             file.close()
         return min
+
+    def clearQueue(self):
+        file = open(self.__filepath, "w")
+        file.close()
 
 #this is for testing
 # def main():
@@ -45,10 +64,19 @@ class Queue ():
 #     test.enqueue(4)
 #     test.enqueue(2)
 #     test.enqueue(3)
+#     test.enqueue(0)
+#     test.enqueue(-1)
 
+#     #test.clearQueue()
+
+#     test.incramentTime()
 #     print(test.dequeue())
+#     test.incramentTime()
 #     print(test.dequeue())
+#     test.incramentTime()
 #     print(test.dequeue())
+#     test.incramentTime()
 #     print(test.dequeue())
+#     test.incramentTime()
 
 # main()
