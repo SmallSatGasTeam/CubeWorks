4f70f5b2bfb749826db23bd22c9abf2ce2fed77a
Joshua Hessing <jjhjjh7@gmail.com>
2021-05-26 12:36:03 -0600

    Updating setNewTXWindow.c

--- flightLogic/missionModes/transmitting.py
+++ flightLogic/missionModes/transmitting.py
@@ -57,6 +57,7 @@ class Transmitting:
                 print("Time to next window is less than 20 but greater than 0")
                 self.__timeToNextWindow = self.__queue.dequeue(0) - time.time()
                 self.__data = []
+                self.__sendData = []
 
             #data[0] = time of next window, data[1] = duration of window, data[2] = datatype, data[3] = picture number, data[4] = line index
             print("About to hit try.")
