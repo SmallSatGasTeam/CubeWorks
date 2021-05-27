5b72728b754eb45177e5343fa346e27b7c9888e7
Joshua Hessing <jjhjjh7@gmail.com>
2021-05-27 12:43:46 -0600

    Addded headers to antennaDeploy.py

--- flightLogic/missionModes/antennaDeploy.py
+++ flightLogic/missionModes/antennaDeploy.py
@@ -1,3 +1,8 @@
+"""
+Contains the antennaMode class and the functions: run, cancelAllTasks, and
+skipToPostBoom
+"""
+
 import sys
 #import subprocess
 import os
@@ -13,6 +18,10 @@ from TXISR import packetProcessing
 
 
 class antennaMode:
+	"""
+	Deploys the antenna when the battery voltage is high enough.
+	Instantiated in mainFlightLogic.
+	"""
 
 	def __init__(self, saveObject, safeModeObject, transmitObject):
 		self.deployVoltage = 3 #Threshold voltage to deploy
@@ -28,6 +37,11 @@ class antennaMode:
 
 
 	async def run(self):
+		"""
+		Deploys the antenna when the battery voltage is high enough.
+		Runs async tasks pythonInterrupt, collectTTNCData, collectAttitudeData,
+		thresholdcheck, skipToPostBoom, readNextTransferWindow, trasmit
+		"""
 		print('Antenna Deploy Running!')
 		self.__tasks.append(asyncio.create_task(pythonInterrupt.interrupt()))
 		self.__tasks.append(asyncio.create_task(self.__getTTNCData.collectTTNCData(1))) #Antenna deploy is mission mode 1
@@ -38,28 +52,32 @@ class antennaMode:
 		self.__tasks.append(asyncio.create_task(self.__transmit.transmit()))
 		
 		eps=EPS()
+		#If ground station has sent command to skip to post boom
 		if await self.skipToPostBoom():
-			return True
+			return True	#Finish this mode and move on
 		while True: #Runs antenna deploy loop
-			if (eps.getBusVoltage()>self.deployVoltage):
+			if (eps.getBusVoltage()>self.deployVoltage): #If the bus voltage is high enough
 				await asyncio.gather(self.__antennaDeployer.deployPrimary()) #Fire Primary Backup Resistor
 				doorStatus = self.__antennaDoor.readDoorStatus() #Check Door status
-				if doorStatus == (1,1,1,1): #probably need to change this to actually work
+				if doorStatus == (1,1,1,1): #NOTE: probably need to change this to actually work
+					#If ground station has sent command to skip to post boom					
 					if await self.skipToPostBoom():
-						return True
+						return True #Finish this mode and move on
 					self.cancelAllTasks(self.__tasks)
 					print('Doors are open, returning true')
 					return True
 				else:
+					#If ground station has sent command to skip to post boom
 					if await self.skipToPostBoom():
-						return True
+						return True #Finish this mode and move on
 					print('Firing secondary, primary did not work. Returning True')
 					await asyncio.gather(self.__antennaDeployer.deploySecondary())
 					self.cancelAllTasks(self.__tasks)
 					return True
 			else:
+				#If ground station has sent command to skip to post boom
 				if await self.skipToPostBoom():
-					return True
+					return True #Finish this mode and move on
 				if(self.timeWaited > self.maximumWaitTime):
 					self.__safeMode.run(10) #1 hour
 					await asyncio.sleep(5) #This is an artifact of testing, and will not matter for the actual flight software
@@ -70,6 +88,9 @@ class antennaMode:
 					await asyncio.sleep(60)
 
 	def cancelAllTasks(self, taskList):
+		"""
+		Cancels all async tasks created at the beginning of run.
+		"""
 		try:
 			for t in taskList:
 				t.cancel()
@@ -77,9 +98,14 @@ class antennaMode:
 			print("Caught thrown exception in cancelling background task")
 
 	async def skipToPostBoom(self):
+		"""
+		Skips to postBoomDeploy mode if the command is received from the ground
+		station.
+		"""
 		print("Inside skipToPostBoom, skipping value is:", packetProcessing.skippingToPostBoom)
+		#If the command has been received to skip to postBoom
 		if packetProcessing.skippingToPostBoom:
-			self.cancelAllTasks(self.__tasks)
-			return True
+			self.cancelAllTasks(self.__tasks) #Cancel all tasks
+			return True #Finish this mode and move on
 		else:
 			await asyncio.sleep(1)
