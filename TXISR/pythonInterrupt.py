b748239e8ae33242c909bc07b53ff6ad4fdf028f
Joshua Hessing <jjhjjh7@gmail.com>
2021-06-01 10:08:05 -0600

    Testing

--- TXISR/pythonInterrupt.py
+++ TXISR/pythonInterrupt.py
@@ -17,47 +17,31 @@ To defray the possibility of half a packet being in the buffer, any half-packets
 """
 
 async def interrupt():
-	fileChecker.fullReset()
-	try:
-		serialport = serial.Serial('/dev/serial0', 115200) #Open serial port. Currently /dev/serial0, might change to the PL011 port for flight article
-	except Exception as e:
-		print("Failed to open serialport. Exception:", repr(e))
-		serialport = None
-
+	serialport = serial.Serial('/dev/serial0', 115200) #Open serial port. Currently /dev/serial0, might change to the PL011 port for flight article
 	leftovers = '' #Stores any half-packets for evaluation the next loop
 	leftoversEmpty = True
 	gaspacsHex = str(b'GASPACS'.hex())
 	while True:
-		print("Python interrupt.", serialport.in_waiting)
-		try:
-			if serialport.in_waiting: #If there is content in the serial buffer, read it and act on it
-			#if True: #This is a testing line
-				print('Data in waiting')
-				data = str(serialport.read(serialport.in_waiting).hex()) #This produces a list of nibbles (half bytes)
-				data = leftovers + data #Append any leftover data for evaluation
-				if leftovers is not '':
-					leftoverEmpty = False
-					print(leftovers, leftoverEmpty)
-				commands, ax25Packets = [], []
-				commands, ax25Packets, leftovers = parseData(data, gaspacsHex)
-				print("Commands:" + str(commands))
-				print("ax25Packets:" + str(ax25Packets))
-				if leftovers is not '' and leftoversEmpty is False:
-					#Something is sticking around in leftovers, let's clear it
-					#Operates on the assumption that 2 consecutive partial packets is practically impossible
-					leftovers = ''
-				for command in commands:
-					print(command)
-					await packetProcessing.processPacket(command) #Process Command Packets
-				for ax25 in ax25Packets:
-					await packetProcessing.processPacket(ax25) #Process AX.25 Packets
-				print("Made it all the way. Leftovers: ", leftovers)
-				await asyncio.sleep(5)
-			else: #No contents in serial buffer
-				print('buffer empty')
-				await asyncio.sleep(3)
-		except Exception as e:
-			print("Failure to run interrupt. Exception: ", e)
+		if serialport.in_waiting: #If there is content in the serial buffer, read it and act on it
+			print('Data in waiting')
+			data = str(serialport.read(serialport.in_waiting).hex()) #This produces a list of nibbles (half bytes)
+			data = leftovers+data #Append any leftover data for evaluation
+			if leftovers is not '':
+				leftoverEmpty = False
+			commands, ax25Packets = [], []
+			commands, ax25Packets, leftovers = parseData(data, gaspacsHex)
+			if leftovers is not '' and leftoversEmpty is False:
+				#Something is sticking around in leftovers, let's clear it
+				#Operates on the assumption that 2 consecutive partial packets is practically impossible
+				leftovers = ''
+			for command in commands:
+				print(command)
+				await packetProcessing.processPacket(command) #Process Command Packets
+			for ax25 in ax25Packets:
+				await packetProcessing.processPacket(ax25) #Process AX.25 Packets
+			await asyncio.sleep(5)
+		else: #No contents in serial buffer
+			print('buffer empty')
 			await asyncio.sleep(3)
 
 def parseData(data, bracket): #Takes data string, in the form of hex, from async read serial function. Spits out all AX.25 packets and GASPACS packets contained inside, as well as remaining data to be put into the leftovers
