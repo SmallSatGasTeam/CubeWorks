f87c1178d103f513d25af47beec78f279b722e05
ShawnBraden <sbradenjones16@gmail.com>
2021-06-01 16:12:31 +0100

    removing ground station

--- GroundStation/eraseReceivedData.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf Data/Attitude_Data/*
-rm -rf Data/TTNC_Data/*
-rm -rf Data/Deploy_Data/*
-echo "Attitude, TTNC, and Deploy data erased"
--- GroundStation/eraseReceivedPics.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -rf Pictures/Low_Quality/*
-rm -rf Pictures/High_Quality/*
-echo "LQ and HQ Pictures erased"
