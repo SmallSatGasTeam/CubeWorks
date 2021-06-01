d007b0fee6dfc303a1c736ca0c370c4d91f65bd3
Joshua Hessing <jjhjjh7@gmail.com>
2021-06-01 13:56:28 -0600

    Created a new testing file to try and see if we can receive transmissions from the ground station to the stack

---
+++ tests/testReceiving.py
@@ -0,0 +1,21 @@
+import serial
+import asyncio
+import sys
+sys.path.append('../')
+import watchdog.Heartbeat.Heartbeat as heartbeat
+
+async def receive():
+    heartbeat.setUp()
+    asyncio.create_task(heartbeat.longTap())
+
+    serialport = serial.Serial('/dev/serial10', 115200)
+
+    while True:
+        print(serialport.in_waiting(), "objects in waiting.")
+        
+        if serialport.in_waiting:
+            print("Data in waiting")
+            data = serialport.read_all()
+            print("Received: ", data)
+        
+        await asyncio.sleep(.5)
