b5fd0752159c17a1e302054605eec4e2780e8655
aallgrunn <aallgrunn@hotmail.com>
2021-06-02 12:16:31 -0600

    Resolved merge conflict

--- TXISR/pythonInterrupt.py
+++ TXISR/pythonInterrupt.py
@@ -63,11 +63,7 @@ async def interrupt():
 				
 			# serialport.close()
 		except Exception as e:
-<<<<<<< HEAD
-			print("Failure to run interrupt. Exception:", e)
-=======
 			print("Failure to run interrupt. Exception:", repr(e))
->>>>>>> 16ea87d7404b72da375d40753bbac29465345fd0
 			await asyncio.sleep(3)
 
 def parseData(data, bracket): #Takes data string, in the form of hex, from async read serial function. Spits out all AX.25 packets and GASPACS packets contained inside, as well as remaining data to be put into the leftovers
