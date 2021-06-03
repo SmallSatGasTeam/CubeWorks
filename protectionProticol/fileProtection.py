de2db91e54a524bd7082dde7aa552e4bef4c4718
evanmand <evanmand02@gmail.com>
2021-06-03 13:00:26 -0600

    Removed comments from window checker

--- protectionProticol/fileProtection.py
+++ protectionProticol/fileProtection.py
@@ -157,7 +157,6 @@ class FileReset():
 
             # This is the TX flag, don't put limits on it
             TXflagStripped = window[4].strip('\n')
-            print("STRIP: ", TXflagStripped)
             if (not isinstance(int(TXflagStripped), int)) or int(window[4]) < -1:
                 TXwindows[count] = ""
                 continue
