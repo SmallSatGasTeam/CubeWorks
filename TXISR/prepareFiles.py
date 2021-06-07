fe4b044b97b520db56ee7b072ac554e96a07de9a
Joshua Hessing <jjhjjh7@gmail.com>
2021-06-07 10:30:13 -0600

    Trying to get this enumerate thing to work

--- TXISR/prepareFiles.py
+++ TXISR/prepareFiles.py
@@ -75,8 +75,9 @@ def prepareData(duration, dataType, startFrom):
 		lineNumber = 0
 		try:
 			for index, line in enumerate(dataFile):
-				print("Index:", index, "Line:", int(line[1:9]))
+				print("Index:", index, "Line:", int(line[1:9]), "Searching for:", transmissionProgress)
 				if int(line[1:9]) == transmissionProgress:
+					print("Found the correct line")
 					lineNumber = int(index) + 1
 		except Exception as e:
 			print("Error:", e)
