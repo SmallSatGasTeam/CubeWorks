834b99a90979a97f3c235728743e2f6da4e29d3e
ShawnBraden <sbradenjones16@gmail.com>
2021-06-10 12:03:39 -0600

    fixing transmit.py

--- flightLogic/missionModes/transmitting.py
+++ flightLogic/missionModes/transmitting.py
@@ -34,7 +34,6 @@ class Transmitting:
         fileChecker.checkFile(self.__txWindowsPath)
         self.__queue = Queue(self.__txWindowsPath)
         self.__timeToNextTXwindowVar = 3133728366
-        self.__nextWindowTime = -1
         self.__duration = -1
         self.__datatype = -1
         self.__pictureNumber = -1
@@ -50,71 +49,20 @@ class Transmitting:
         then dequeues a transfer window once within 20 seconds of that timestamp
         """
         while True:
-            print("INSIDE TRANSFER WINDOW")
-            #read the given transfer window file and extract the data for the soonest transfer window
-            soonestWindowTime = 0
-
-            #while timestamp < currenttimestamp
-            while (self.__queue.dequeue(0) < time.time()) and (self.__queue.dequeue(0) != -1):
-                fileChecker.windowProtection()
-                self.__queue.dequeue(1)
-            #20 seconds before
-            if ((self.__queue.dequeue(0) - time.time() <= 20) and 
-                (self.__data == []) and (self.__queue.dequeue(0) > 0) 
-                and (not self.__inProgress)):
-                #pull the packet
-                fileChecker.windowProtection()
-                line = self.__queue.dequeue(1)
-                self.__sendData = line.split(',')
-            #If not within 20 seconds of the next time stamp
-            elif ((self.__timeToNextTXwindowVar < 0) or (self.__timeToNextTXwindowVar > 20)) and (self.__queue.dequeue(0) != -1):
-                #Reset data and sendData lists, pull the time till next window from the next element in the queue
-                fileChecker.windowProtection()
-                self.__sendData = []
-
-            #If sendData has the right number of members
-            if self.__sendData.__len__() == 5:
-                print(self.__sendData)
-                #Assign the variables appropriately
-                try :
-                    self.__timeToNextTXwindowVar = float(self.__sendData[0]) - time.time()
-                    self.__duration = int(self.__sendData[1])
-                    self.__datatype = int(self.__sendData[2])
-                    self.__pictureNumber = int(self.__sendData[3])
-                    self.__nextWindowTime = float(self.__sendData[0])
-                    self.__index = int(self.__sendData[4])
-                except :
-                    print("Data window is unreadable")
-            else:
-                print("sendData is empty.")
-
-            if (not self.__inProgress) and (self.__sendData != []):
-                asyncio.tasks.create_task(self.transmissionRunning())
-
-            await asyncio.sleep(5)
-    
-    async def transmit(self):
-        """
-        Prepares data to be sent once under 14 seconds and then runs 
-        TXService.run once under 5 seconds to transmit the prepared data.
-        """
-        while True:
             while True:
-                print("Transmit time to next window:", self.__timeToNextTXwindowVar)
                 #if close enough, prep files
                 #wait until 5 seconds before, return True
-                if (self.__timeToNextTXwindowVar != -1) and (self.__timeToNextTXwindowVar < 15) and (self.__timeToNextTXwindowVar >= 0):
-                    if self.__datatype < 3: #Attitude, TTNC, or Deployment data respectively
+                if(self.__timeToNextTXwindowVar is not -1 and self.__timeToNextTXwindowVar<20): #If next window is in 2 minutes or less
+                    #get the data 
+                    self.__sendData = self.__queue.dequeue(True)
+                    if self.__datatype < 3:#Attitude, TTNC, or Deployment data respectively
                         prepareFiles.prepareData(self.__duration, self.__datatype, self.__index)
                     else:
                         print("Transimtting.py:", self.__duration, self.__datatype, self.__pictureNumber)
                         prepareFiles.preparePicture(self.__duration, self.__datatype, self.__pictureNumber, self.__index)
                     break
-                 #I decearsed the wait time because we were missing windows.
                 await asyncio.sleep(5)
             while True:
-                #print("Is it time?")
-                #I added a neg time buff as well incase we are a little late gettering here
                 if (self.__timeToNextTXwindowVar <= 5) and (self.__timeToNextTXwindowVar > -5):
                     fileChecker.checkFile('/home/pi/TXISRData/transmissionsFlag.txt')
                     self.__transmissionFlagFile.seek(0)
@@ -125,12 +73,11 @@ class Transmitting:
                         #print("We should literally be running this.")
                         subprocess.Popen(['sudo', './TXService.run', str(self.__datatype)], cwd = str(txisrCodePath))
                         #os.system("cd ; cd " + str(txisrCodePath) + " ; sudo ./TXService.run " + str(self.__datatype)
-                        break
                     else:
                         print("Transmission flag is not enabled")
-
-                await asyncio.sleep(.01)
-
+                    break
+                await asyncio.sleep(0.1) #Check at 10Hz until the window time gap is less than 5 seconds	
+    
     async def transmissionRunning(self):
         self.__inProgress = True
         await asyncio.sleep(self.__duration + 5)
@@ -138,10 +85,10 @@ class Transmitting:
 
     async def upDateTime(self):
         #count down the time
-        if (self.__queue.dequeue(0) != -1):
-            self.__timeToNextTXwindowVar = self.__queue.dequeue(0) - time.time()
+        if (self.__queue.dequeue(False) != -1):
+            self.__timeToNextTXwindowVar = self.__queue.dequeue(False) - time.time()
         #other wise set the time to the defualt vaule
-        elif (self.__queue.dequeue(0) == -1) and (self.__queue.dequeue(0) - time.time()) > -10:
+        elif (self.__queue.dequeue(False) == -1) and (self.__timeToNextTXwindowVar - time.time()) < -10:
                 self.__timeToNextTXwindowVar = 3133728366
         print("Time to next window:", self.__timeToNextTXwindowVar)
         await asyncio.sleep(2.5)
