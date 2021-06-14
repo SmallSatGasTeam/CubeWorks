75ffc9d66b6b331823dbc51cfe3b20fe8f088b43
Daniel Combs <danielw.combs.jr@aggiemail.usu.edu>
2021-06-14 08:34:30 -0600

    TYPO

--- flightLogic/missionModes/transmitting.py
+++ flightLogic/missionModes/transmitting.py
@@ -98,7 +98,7 @@ class Transmitting:
                         startTime = time.time()
                         subprocess.Popen(['sudo', './TXService.run', str(self.__datatype)], cwd = str(txisrCodePath))
                         while(self.__duration >= time.time() - startTime):
-                            print("\tWaiting for TX to finsih", self.__duration >= time.time() - startTime)
+                            print("\tWaiting for TX to finish", self.__duration >= time.time() - startTime)
                             await asyncio.sleep(2)
                         #os.system("cd ; cd " + str(txisrCodePath) + " ; sudo ./TXService.run " + str(self.__datatype)
                     else:
