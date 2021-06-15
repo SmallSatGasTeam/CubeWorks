e8aa975a15cd8e437b30b3ef2ad8c63e17fe7b7c
Daniel Combs <danielw.combs.jr@aggiemail.usu.edu>
2021-06-15 13:06:25 -0600

    no comment

--- TXISR/packetProcessing.py
+++ TXISR/packetProcessing.py
@@ -389,7 +389,8 @@ class packetProcessing:
 
 
 	def deleteData(self):
-		os.system("cd flightLogicData/; sudo rm Attitude_Data.txt bootRecords.txt backupBootRecords.txt Deploy_Data.txt TTNC_Data.txt")
+		os.system("pwd")
+		os.system("cd home/pi/flightLogicData/; sudo rm Attitude_Data.txt bootRecords.txt backupBootRecords.txt Deploy_Data.txt TTNC_Data.txt")
 
 	def enableAudioBeacon(self):
 		pass
