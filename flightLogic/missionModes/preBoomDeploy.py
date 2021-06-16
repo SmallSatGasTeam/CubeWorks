6a95d2cbd46fbdf4eb5872a570da454a026a3e53
ShawnBraden <sbradenjones16@gmail.com>
2021-06-16 09:18:35 -0600

    fixing a print statement

--- flightLogic/missionModes/preBoomDeploy.py
+++ flightLogic/missionModes/preBoomDeploy.py
@@ -97,7 +97,6 @@ class preBoomMode:
 
 		while True:
 			print("checking for sunlight")
-			print("Returned value: " +  self.sunlightData)
 			if ((self.sunlightData > self.darkVoltage) and self.batteryStatusOk == True):
 				self.cancelAllTasks(self.__tasks) #Cancel all background processes, this depolys the boom basically
 				print('Returning and exiting')
