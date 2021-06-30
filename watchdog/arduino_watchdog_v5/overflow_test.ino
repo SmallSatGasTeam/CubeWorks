fde9cbbe2996d57695d3fda63f14ae54b97401e3
Benjamin Lewis <46497493+BenJayLew@users.noreply.github.com>
2021-06-30 10:05:02 -0600

    Oserflow test for testing the wait function.

---
+++ watchdog/arduino_watchdog_v5/overflow_test.ino
@@ -0,0 +1,44 @@
+#include<avr/wdt.h>
+
+//Custom wait function that acts like delay, but does not time out the internal watchdog
+void wait(long delayTime){
+  long timer_start = millis() + 2147453000;
+  long timer = millis() + 2147453000;
+  while(timer_start + delayTime >= timer) {
+    wdt_enable(WDTO_8S);
+    timer = millis() + 2147453000;
+
+    //digitalWrite(LED, HIGH);
+    Serial.print("Timer start: ");
+    Serial.print(timer_start);
+    Serial.print("\t\tTimer: ");
+    Serial.print(timer);
+    Serial.print("\t\tdelay time: ");
+    Serial.println(delayTime);
+    
+    //digitalWrite(LED, LOW);
+    
+    //check for overflow
+    if(timer_start > timer + 10){
+      Serial.println("Int overflow tripped, resetting timers");
+
+      //Experimental code, needs testing 
+      delayTime -= (2147483647 - timer_start); //length of a long minus the timer start. Gives the time we already waited and subtracts that from the delay variable 
+      
+      
+      timer_start = millis();
+      timer = millis();
+    }
+  }
+}
+
+void setup() {
+  wdt_disable();
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  wait(60000);
+}
