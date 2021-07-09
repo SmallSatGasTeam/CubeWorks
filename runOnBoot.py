5b51fd6164d6cc65018e59c911bdfeabfe053d9e
ShawnBraden <sbradenjones16@gmail.com>
2021-07-09 10:03:05 -0600

    removing unneeded code

--- runOnBoot.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-with open("log.txt", "a") as f:
-    f.write("Booted\n")
-
-cmd = 'python3 watchdog/Heartbeat/testHeartbeat.py &'
-
-p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
