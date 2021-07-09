f5a89ab665980945bda8fc230904cc3400435913
ShawnBraden <sbradenjones16@gmail.com>
2021-07-09 09:48:12 -0600

    removing unneeded code

--- watchdog/Heartbeat/testHeartbeat.py
+++ watchdog/Heartbeat/testHeartbeat.py
@@ -1,10 +1,8 @@
-import Heartbeat
 import asyncio
 from flightLogic.missionModes.heartBeat import heart_beat
 
 """ Tests if heartbeat is working """
 
-Heartbeat.setUp()
 heartBeatObj = heart_beat()
 print("Starting receive.")
 asyncio.create_task(heartBeatObj.heartBeatRun())
