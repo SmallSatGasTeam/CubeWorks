596113c84f02c0dc60701d5d4bfe7ed4fce4715d
ShawnBraden <sbradenjones16@gmail.com>
2021-07-27 10:50:40 -0600

    working on the antenna driver

--- flightLogic/mainFlightLogic.py
+++ flightLogic/mainFlightLogic.py
@@ -96,7 +96,7 @@ async def executeFlightLogic():  # Open the file save object, start TXISR, camer
 		while(transmitObject.isRunning()):
 			await asyncio.sleep(60) #sleep if a transmission is running
 
-	print('Moving on to check antenna door status')
+	print("Moving on to check antenna door status")
 	#deploy the antenna, if it fails we will do nothing
 	try: 
 		antennaDoorObj.deployAntennaMain() #wait for the antenna to deploy
