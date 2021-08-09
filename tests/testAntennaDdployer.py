66ba053db51ac941b43a70f5ae02487003f4aefc
ShawnBraden <sbradenjones16@gmail.com>
2021-08-09 09:45:09 -0600

    adding deploy antenna test

--- tests/testAntennaDdployer.py
+++ tests/testAntennaDdployer.py
@@ -4,6 +4,9 @@ from Drivers.antennaDoor import AntennaDoor
 from time import sleep
 
 antennaDoor = AntennaDoor()
+print("WARNING THIS WILL DEPLOY THE ANTENNA!!!!") 
+for i in range(10):
+    print("Count down: ", 10 - i)
 while True:
     print(antennaDoor.readDoorStatus())
     sleep(1)
