721c7cfd31a252f2dbc290c25b9f347b1ac58ffe
ShawnBraden <sbradenjones16@gmail.com>
2021-08-10 17:36:07 -0600

    adding some stuff to the test

--- tests/testBackupAntennaDeployer.py
+++ tests/testBackupAntennaDeployer.py
@@ -1,11 +1,13 @@
 import sys
 sys.path.append('../')
+import time
 from Drivers.backupAntennaDeployer import BackupAntennaDeployer
 import asyncio
 
 print("WARNING THIS WILL DEPLOY THE ANTENNA!!!!") 
 for i in range(10):
     print("Count down: ", 10 - i)
+    time.sleep(1)
 antennaDeployTest = BackupAntennaDeployer()
 asyncio.run(antennaDeployTest.deployPrimary())
 print('Primary Done')
