51387909096bf5cc3d720a4735663e21311b0906
ShawnBraden <sbradenjones16@gmail.com>
2021-08-10 18:49:31 -0600

    fixing testing code

--- tests/testPrimaryAntennaDeploy.py
+++ tests/testPrimaryAntennaDeploy.py
@@ -9,8 +9,8 @@ print("WARNING THIS WILL DEPLOY THE ANTENNA!!!!")
 for i in range(10):
     print("Count down: ", 10 - i)
     sleep(1)
-while True:
-    print(antennaDoor.readDoorStatus())
-    sleep(1)
-    antennaDoor.deployAntennaMain()
-    sleep(1)
+
+print(antennaDoor.readDoorStatus())
+sleep(1)
+antennaDoor.deployAntennaMain()
+sleep(1)
